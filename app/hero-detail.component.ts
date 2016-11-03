fd7436d74c8edccb48b0fa7028b0dcdd8363f320
zarlex <alex.zarges@online.de>
2016-11-03 17:01:30 +0100

    add functionality to add, modify, delete hero

--- app/hero-detail.component.ts
+++ app/hero-detail.component.ts
@@ -30,4 +30,9 @@ export class HeroDetailComponent implements OnInit {
     goBack(): void {
         this.location.back();
     }
+
+    save(): void{
+        this.heroService.update(this.hero)
+            .then(() => this.goBack());
+    }
 }
--- app/hero.service.ts
+++ app/hero.service.ts
@@ -4,12 +4,18 @@ import { Headers, Http } from '@angular/http';
 import 'rxjs/add/operator/toPromise';
 
 import {Hero} from './hero';
-import {HEROES} from './mock-heroes';
 
 @Injectable()
 export class HeroService {
 
     private heroesUrl = 'app/heroes';
+
+    private headers = new Headers({'Content-Type': 'application/json'});
+
+    private handleError(error: any): Promise<any> {
+        console.error('An error occurred', error); // for demo purposes only
+        return Promise.reject(error.message || error);
+    }
 
     constructor(private http: Http) { }
 
@@ -24,4 +30,30 @@ export class HeroService {
         return this.getHeroes()
             .then(heroes => heroes.find(hero => hero.id === id));
     }
+
+    update(hero: Hero): Promise<Hero> {
+        const url = `${this.heroesUrl}/${hero.id}`;
+        return this.http
+            .put(url, JSON.stringify(hero), {headers: this.headers})
+            .toPromise()
+            .then(() => hero)
+            .catch(this.handleError);
+    }
+
+    create(name: string): Promise<Hero> {
+        return this.http
+            .post(this.heroesUrl, JSON.stringify({name: name}), {headers: this.headers})
+            .toPromise()
+            .then(res => res.json().data)
+            .catch(this.handleError);
+    }
+
+    delete(id: number): Promise<void> {
+        const url = `${this.heroesUrl}/${id}`;
+        return this.http.delete(url, {headers: this.headers})
+            .toPromise()
+            .then(() => null)
+            .catch(this.handleError);
+    }
 }
+
