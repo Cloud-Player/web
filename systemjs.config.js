523d19f729a170468eece514b9e78cf974a4ecb0
zarlex <alex.zarges@online.de>
2016-11-06 21:03:54 +0100

    include backbone and underscore with its typescript definitions

--- systemjs.config.js
+++ systemjs.config.js
@@ -25,8 +25,12 @@
       '@angular/upgrade': 'npm:@angular/upgrade/bundles/upgrade.umd.js',
 
       // other libraries
-      'rxjs':                      'npm:rxjs',
-      'angular-in-memory-web-api': 'npm:angular-in-memory-web-api/bundles/in-memory-web-api.umd.js'
+      'rxjs': 'npm:rxjs',
+      'angular-in-memory-web-api': 'npm:angular-in-memory-web-api/bundles/in-memory-web-api.umd.js',
+
+      'underscore': 'npm:underscore/underscore.js',
+      'backbone': 'npm:backbone/backbone.js',
+      'jquery': 'npm:jquery/dist/jquery.js'
     },
     // packages tells the System loader how to load when no filename and/or no extension
     packages: {
