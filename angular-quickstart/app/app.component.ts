ad61c44f6312c75a2df5c6faece04e5e293df9f6
Kai Schulz <schulzkai@googlemail.com>
2016-11-17 20:19:14 +0100

    completed angular2 tutorial "introduction" and "the hero editor" - https://angular.io/docs/ts/latest/tutorial/toh-pt1.html

--- angular-quickstart/app/app.component.ts
+++ angular-quickstart/app/app.component.ts
@@ -1,6 +1,27 @@
 import { Component } from '@angular/core';
+
 @Component({
     selector: 'my-app',
-    template: '<h1>Hello Angular!</h1>'
+    template:`
+        <h1>{{title}}</h1>
+        <h2>{{hero.name}} details!</h2>
+        <div><label>id: </label>{{hero.id}}</div>
+        <div>
+            <label>name: </label>
+            <input [(ngModel)]="hero.name" placeholder="name">
+        </div>
+        `
 })
-export class AppComponent { }
+
+export class AppComponent {
+    title = 'Tour of Heroes';
+    hero: Hero = {
+        id: 1,
+        name: 'Windstorm'
+    };
+}
+
+export class Hero {
+    id: number;
+    name: string;
+}
--- angular-quickstart/app/app.module.ts
+++ angular-quickstart/app/app.module.ts
@@ -1,11 +1,20 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {AppComponent} from "./app.component";
+import { FormsModule }   from '@angular/forms';
+
+import { AppComponent } from "./app.component";
 
 @NgModule({
-    imports:      [ BrowserModule ],
-    declarations: [ AppComponent ],
-    bootstrap:    [ AppComponent ]
+    imports:      [
+        BrowserModule,
+        FormsModule
+    ],
+    declarations: [
+        AppComponent
+    ],
+    bootstrap:    [
+        AppComponent
+    ]
 })
 
 export class AppModule { }
