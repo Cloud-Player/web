2416b96713f47b03794f8c3df0b9a18bc5e79041
Kai Schulz <schulzkai@googlemail.com>
2016-11-17 23:13:41 +0100

    completed angular2 tutorial "services" - https://angular.io/docs/ts/latest/tutorial/toh-pt4.html

--- angular-quickstart/app/app.component.ts
+++ angular-quickstart/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
 
 import { Hero } from './hero'
+import { HeroService } from './hero.service'
 
 @Component({
     selector: 'my-app',
@@ -64,30 +66,30 @@ import { Hero } from './hero'
         margin-right: .8em;
         border-radius: 4px 0 0 4px;
       }
-    `]
+    `],
+    providers: [HeroService]
 })
 
-export class AppComponent {
+export class AppComponent implements OnInit{
 
     title = 'Tour of Heroes';
-    heroes = HEROES;
+    heroes: Hero[];
     selectedHero: Hero;
+
+    constructor(private heroService: HeroService) {
+
+    }
+
+    getHeros(): void {
+        this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+    }
 
     onSelect(hero: Hero): void {
         this.selectedHero = hero;
     }
 
-}
+    ngOnInit(): void {
+        this.getHeros()
+    }
 
-const HEROES: Hero[] = [
-    { id: 11, name: "Mr. Nice"},
-    { id: 12, name: "Narco"},
-    { id: 13, name: "Bombasto"},
-    { id: 14, name: "Celeritas"},
-    { id: 15, name: "Magneta"},
-    { id: 16, name: "RubberMan"},
-    { id: 17, name: "Dynama"},
-    { id: 18, name: "Dr IQ"},
-    { id: 19, name: "Magma"},
-    { id: 20, name: "Tornado"},
-];
+}
