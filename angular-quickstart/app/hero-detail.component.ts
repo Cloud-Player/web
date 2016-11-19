3161401b73de4b1ad85ed5ba83c05870b5314e4f
Kai Schulz <schulzkai@googlemail.com>
2016-11-19 14:48:09 +0100

    completed angular2 tutorial "HTTP" first half - https://angular.io/docs/ts/latest/tutorial/toh-pt6.html

--- angular-quickstart/app/hero-detail.component.ts
+++ angular-quickstart/app/hero-detail.component.ts
@@ -34,4 +34,10 @@ export class HeroDetailComponent implements OnInit {
         this.location.back();
     }
 
+    save(): void {
+        this.heroService.update(this.hero)
+            .then(() => this.goBack());
+    }
+
+
 }
--- angular-quickstart/app/heroes.component.ts
+++ angular-quickstart/app/heroes.component.ts
@@ -38,4 +38,23 @@ export class HeroesComponent implements OnInit {
         this.getHeros()
     }
 
+    add(name: string): void {
+        name = name.trim();
+        if (!name) { return; }
+        this.heroService.create(name)
+            .then(hero => {
+                this.heroes.push(hero);
+                this.selectedHero = null;
+            });
+    }
+
+    delete(hero: Hero): void {
+        this.heroService
+            .delete(hero.id)
+            .then(() => {
+                this.heroes = this.heroes.filter(h => h !== hero);
+                if (this.selectedHero === hero) { this.selectedHero = null; }
+            });
+    }
+
 }
