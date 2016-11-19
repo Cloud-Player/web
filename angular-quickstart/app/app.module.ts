00388c11d1c64d5dd4536a3af457b7913e8e13eb
Kai Schulz <schulzkai@googlemail.com>
2016-11-19 16:04:55 +0100

    completed angular2 tutorial "HTTP" second half - https://angular.io/docs/ts/latest/tutorial/toh-pt6.html

--- angular-quickstart/app/app.module.ts
+++ angular-quickstart/app/app.module.ts
@@ -1,3 +1,5 @@
+import './rxjs-extensions';
+
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
@@ -10,8 +12,9 @@ import { InMemoryDataService }  from './in-memory-data.service';
 import { AppComponent }         from "./app.component";
 import { HeroDetailComponent }  from './hero-detail.component';
 import { HeroesComponent }      from './heroes.component';
-import { HeroService }          from './hero.service';
 import { DashboardComponent }   from './dashboard.component';
+import { HeroSearchComponent }  from './hero-search.component';
+import { HeroService }          from './hero.service';
 
 import { AppRoutingModule }     from './app-routing.module';
 
@@ -27,7 +30,8 @@ import { AppRoutingModule }     from './app-routing.module';
         AppComponent,
         DashboardComponent,
         HeroDetailComponent,
-        HeroesComponent
+        HeroesComponent,
+        HeroSearchComponent
     ],
     providers: [
         HeroService
