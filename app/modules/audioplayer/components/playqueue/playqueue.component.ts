5c7ee178ac2991f28b27f2f7604650674692a06f
Kai Schulz <mail@kaischulz.com>
2016-11-30 14:14:51 +0100

    added play queue scaffolding

---
+++ app/modules/audioplayer/components/playqueue/playqueue.component.ts
@@ -0,0 +1,23 @@
+/**
+ * Created by kaischulz on 30.11.16.
+ */
+
+import {Component, OnInit} from '@angular/core';
+
+@Component ({
+    moduleId: module.id,
+    selector: 'play-queue',
+    templateUrl: 'playqueue.template.html',
+    styleUrls: ['playqueue.style.css']
+
+})
+
+export class PlayQueueComponent implements OnInit{
+
+    constructor() {
+    }
+
+    ngOnInit(): void {
+    }
+
+}
