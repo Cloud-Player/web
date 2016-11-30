0237e9f9c3491dd13daf7d683bd383581f1c2d64
Kai Schulz <mail@kaischulz.com>
2016-11-30 12:36:28 +0100

    added more basic audio features like back, pause, next, volume up, volume down

--- app/modules/audioplayer/components/index/index.component.ts
+++ app/modules/audioplayer/components/index/index.component.ts
@@ -11,22 +11,52 @@ import {Config} from "../../../../config/config";
 
 export class AudioPlayerComponent implements OnInit{
     title: string = "This is our awesome Soundcloud Audioplayer!!!"
+
     private audio: any;
+    private playList: string[];
 
     constructor() {
     }
 
     ngOnInit(): void {
         this.audio = new Audio();
-        this.audio.src = Config.testSongURL;
+        this.playList = Config.testPlaylist;
+        this.audio.src = this.playList[0];
     }
 
     playSong(): void {
         this.audio.play();
     }
 
-    stopSong(): void {
+    pauseSong(): void {
         this.audio.pause();
+    }
+
+    previousSong(): void {
+        var index: any = this.playList.indexOf(this.audio.src);
+        this.audio.src = this.playList[index - 1];
+        this.audio.play();
+    }
+
+    nextSong(): void {
+        var index: any = this.playList.indexOf(this.audio.src);
+        this.audio.src = this.playList[index + 1];
+        this.audio.play();
+    }
+
+    turnVolumeUp(): void {
+        console.log(this.audio.volume);
+        this.audio.volume += 0.1;
+    }
+
+    turnVolumeDown(): void {
+        console.log(this.audio.volume);
+        this.audio.volume -= 0.1;
+    }
+
+    onSongSelect(song: string): void {
+        this.audio.src = song;
+        this.audio.play();
     }
 
 }
