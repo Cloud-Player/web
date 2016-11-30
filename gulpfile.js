9f9761dfa1de2033907fd9c36c15d795881f4400
Daniel Reißner <mail@danielreissner.com>
2016-11-30 15:27:46 +0100

    Modified gulpfile to fix clean task and added dependency to package.json

--- gulpfile.js
+++ gulpfile.js
@@ -5,44 +5,37 @@ var clean = require('gulp-clean');
 var gulp = require('gulp');
 var gutil = require('gulp-util');
 var htmlhint = require("gulp-htmlhint");
+var runSequence = require('run-sequence');
 var sass = require('gulp-sass');
 var sasslint = require('gulp-sass-lint');
 var ts = require('gulp-typescript');
 var typescript = require('typescript');
 var tslint = require('gulp-tslint');
 
-gulp.task('default', ['clean', 'build', 'watch']);
+// main tasks
+
+gulp.task('default', ['clean'], function () {
+    gulp.start(['build', 'watch']);
+});
 
 gulp.task('build', ['res:copy', 'html:copy', 'js:build', 'css:build']);
 
-gulp.task('clean', ['clean:dist']);
+// clean tasks
 
-gulp.task('css:build', ['scss:lint'], function () {
-    return gulp.src('./app/**/*.scss')
-        .pipe(sass({ outputStyle: 'compressed' })
-            .on('error', sass.logError))
-        .pipe(gulp.dest('./dist/app')
-            .on('error', gutil.log));
-});
-
-gulp.task('js:build', ['ts:lint'], function () {
-    var tsProject = ts.createProject('./tsconfig.json', {
-        typescript: typescript
+gulp.task('clean', function (done) {
+    return runSequence('clean:dist', function () {
+        done();
     });
-
-    return gulp.src('./app/**/*.ts')
-        .pipe(tsProject().on('error', gutil.log))
-        .pipe(gulp.dest('./dist/app'));
 });
 
-gulp.task('html:copy', function () {
-    return gulp.src('./app/**/*.html')
-        .pipe(gulp.dest('./dist/app')
-            .on('error', gutil.log));
-
+gulp.task('clean:dist', function () {
+    return gulp.src('./dist/*', { read: false })
+        .pipe(clean({ force: true }));
 });
 
-gulp.task('res:copy', function () {
+// build tasks
+
+gulp.task('res:copy',  function () {
     return gulp.src([
         './index.html',
         './systemjs.config.js',
@@ -52,16 +45,40 @@ gulp.task('res:copy', function () {
 
 });
 
+gulp.task('html:copy', function () {
+    return gulp.src('./app/**/*.html', { base: '.' })
+        .pipe(gulp.dest('./dist')
+            .on('error', gutil.log));
+
+});
+
+gulp.task('css:build', ['scss:lint'], function () {
+    return gulp.src('./app/**/*.scss', { base: '.' })
+        .pipe(sass({ outputStyle: 'compressed' })
+            .on('error', sass.logError))
+        .pipe(gulp.dest('./dist')
+            .on('error', gutil.log));
+});
+
+gulp.task('js:build', ['ts:lint'], function () {
+    var tsProject = ts.createProject('./tsconfig.json', {
+        typescript: typescript
+    });
+
+    return gulp.src('./app/**/*.ts', { base: '.' })
+        .pipe(tsProject().on('error', gutil.log))
+        .pipe(gulp.dest('./dist'));
+});
+
+// watch tasks
+
 gulp.task('watch', ['build'], function() {
     gulp.watch('./app/**/*.html', ['html:copy']);
     gulp.watch('./app/**/*.scss', ['css:build']);
     gulp.watch('./app/**/*.ts', ['js:build']);
 });
 
-gulp.task('clean:dist', function () {
-    return gulp.src('./dist/**/*.*', { read: false })
-        .pipe(clean({ force: true }));
-});
+// lints & hints
 
 gulp.task('ts:lint', function() {
     return gulp.src('./app/**/*.ts')
