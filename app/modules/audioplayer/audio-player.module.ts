f9b0c8c53c6578439c041c3b82eee82279e97e76
Kai Schulz <mail@kaischulz.com>
2016-11-30 15:51:29 +0100

    removed audio-player routes - not needed

--- app/modules/audioplayer/audio-player.module.ts
+++ app/modules/audioplayer/audio-player.module.ts
@@ -7,15 +7,13 @@ import {FormsModule} from "@angular/forms";
 
 import {BrowserModule} from "@angular/platform-browser";
 import {AudioPlayerComponent} from "./components/index/index.component";
-import {AudioPlayerRoutingModule} from "./audio-player.routes";
 import {PlayQueueComponent} from "./components/playqueue/playqueue.component";
 import {PlayerControlsComponent} from "./components/controls/controls.component";
 
 @NgModule ({
     imports: [
         BrowserModule,
-        FormsModule,
-        AudioPlayerRoutingModule
+        FormsModule
     ],
     declarations: [
         AudioPlayerComponent,
