3cc683fbf685d05c3c97a1653d608f20e699dcde
Daniel Reißner <mail@danielreissner.com>
2016-12-02 14:56:07 +0100

    Add new systemjs config for the gulp bundle task and renamed other config files

--- systemjs.config.dev.js
+++ systemjs.config.dev.js
@@ -11,7 +11,7 @@
     // map tells the System loader where to look for things
     map: {
       // our app is within the app folder
-      app: 'dist/prod/app',
+      app: 'app',
 
       // angular bundles
       '@angular/core': 'npm:@angular/core/bundles/core.umd.js',
