af93af18f5598ec55926ede68b62a0c5312348a6
Zarlex <alex.zarges@online.de>
2016-12-07 13:35:58 +0100

    add callback component that is called when the soundcloud connect was successful

---
+++ app/modules/session/components/soundcloud-callback/soundcloud_callback.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'soundcloud-callback',
+  templateUrl: 'soundcloud_callback.template.html',
+  styleUrls: ['soundcloud_callback.style.css']
+})
+export class SoundcloudCallbackComponent {
+
+}
