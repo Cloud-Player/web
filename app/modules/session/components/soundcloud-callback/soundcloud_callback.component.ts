674c24ebfd5d8ee04c8a1f4e872fd38bd2487cd9
Zarlex <alex.zarges@online.de>
2016-12-14 11:05:16 +0100

    add session

--- app/modules/session/components/soundcloud-callback/soundcloud_callback.component.ts
+++ app/modules/session/components/soundcloud-callback/soundcloud_callback.component.ts
@@ -1,5 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute, Params} from '@angular/router';
+import {Session} from '../../models/session.model';
+import {User} from '../../../users/models/user.model';
 
 @Component({
   moduleId: module.id,
@@ -8,13 +10,25 @@ import {ActivatedRoute, Params} from '@angular/router';
   styleUrls: ['soundcloud_callback.style.css']
 })
 export class SoundcloudCallbackComponent implements OnInit {
+  private session = Session.getInstance();
 
   constructor(private route: ActivatedRoute) {
+
   };
 
   ngOnInit(): void {
+    this.session.get('user').on('change:authenticated', (user: User) => {
+      user.fetch().then(() => {
+        debugger;
+      });
+    });
+
     this.route.queryParams.forEach((params: Params) => {
-      debugger;
+      this.session.set({
+        access_token: params.access_token,
+        expires_in: params.expires_in,
+        refresh_token: params.refresh_token
+      });
     });
   };
 }
--- app/modules/session/models/session.model.ts
+++ app/modules/session/models/session.model.ts
@@ -1,18 +1,54 @@
 import {SoundcloudModel} from '../../main/models/soundcloud.model';
 import {Injectable} from '@angular/core';
+import {User} from '../../users/models/user.model';
+import {AuthenticatedUser} from './authenticated_user.model';
+import {Http} from '@angular/http';
 
 @Injectable()
 export class Session extends SoundcloudModel {
   idAttribute = 'access_token';
+
+  private static instance: Session;
+
+  static getInstance(): SoundcloudModel {
+    if (!Session.instance) {
+      Session.instance = new Session();
+    }
+    return Session.instance;
+  }
 
   defaults() {
     return {
       expires_in: null,
       refresh_token: null
     };
+  };
+
+  nested() {
+    return {
+      user: AuthenticatedUser
+    };
+  };
+
+  refresh(): any {
+    if (this.get('refresh_token')) {
+      return this.request('//sc.menu-flow.com', 'POST', {
+        data: {
+          refresh_token: this.get('refresh_token')
+        }
+      });
+    } else {
+      return false;
+    }
   }
 
-  refresh(): string {
-    return this.request('http://menu-flow.com','POST')
+  initialize() {
+    this.on('change:access_token', () => {
+      if (this.get('access_token')) {
+        this.get('user').set('authenticated', true);
+      } else {
+        this.get('user').set('authenticated', false);
+      }
+    });
   }
 }
