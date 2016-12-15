68f0f2123c4e620696dd7e1683a1d06a59cdd933
Zarlex <alex.zarges@online.de>
2016-12-15 23:13:00 +0100

    use decorator for authentication logic

--- app/modules/session/models/authenticated_user.model.ts
+++ app/modules/session/models/authenticated_user.model.ts
@@ -1,8 +1,10 @@
 import {Injectable} from '@angular/core';
 import {User} from '../../users/models/user.model';
 import {extend} from 'underscore';
+import {authenticated} from '../decorators/authenticated.decorator';
 
 @Injectable()
+@authenticated
 export class AuthenticatedUser extends User {
   endpoint = '/me';
 
@@ -10,13 +12,6 @@ export class AuthenticatedUser extends User {
     return extend(super.defaults(), {
       authenticated: false
     });
-  }
-
-  sync(method: string, model: AuthenticatedUser, options?: any) {
-    if (this.get('authenticated')) {
-      options.search.set('oauth_token', this.parent.get('access_token'));
-    }
-    return super.sync(method, model, options);
   }
 
 }
