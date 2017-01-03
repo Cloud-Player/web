000bacac9a545f2267bab01b46b1edb63055abbd
Zarlex <alex.zarges@online.de>
2017-01-03 18:12:17 +0100

    add playlists as nested attribute

--- app/modules/session/models/authenticated_user.model.ts
+++ app/modules/session/models/authenticated_user.model.ts
@@ -2,11 +2,18 @@ import {Injectable} from '@angular/core';
 import {User} from '../../users/models/user.model';
 import {extend} from 'underscore';
 import {authenticated} from '../decorators/authenticated.decorator';
+import {AuthenticatedUserPlaylists} from '../collections/authenticated_user_playlists.collection';
 
 @Injectable()
 @authenticated
 export class AuthenticatedUser extends User {
   endpoint = '/me';
+
+  nested() {
+    return {
+      playlists: AuthenticatedUserPlaylists
+    }
+  }
 
   defaults() {
     return extend(super.defaults(), {
