e390f57bc8c853b3795528afc59e3b79f2bb416e
Zarlex <alex.zarges@online.de>
2017-01-04 14:05:36 +0100

    add image model with methods to get image in diffrent sizes

---
+++ app/modules/main/models/soundcloud-image.model.ts
@@ -0,0 +1,23 @@
+import {Injectable} from '@angular/core';
+import {BaseModel} from '../../backbone/models/base.model';
+
+@Injectable()
+export class SoundcloudImageModel extends BaseModel {
+  getImageByFormat(format: string) {
+    if(this.get('id')){
+      return this.get('id').replace('-large', `-${format}`);
+    }
+  };
+
+  getLargeSize() {
+    return this.getImageByFormat('t500x500');
+  };
+
+  getSmallSize(){
+    return this.getImageByFormat('small');
+  };
+
+  getDefaultSize() {
+    return this.get('id');
+  };
+}
