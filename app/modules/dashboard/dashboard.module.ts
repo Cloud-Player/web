1e7a8ef5dfafb1cd97d16c7f79785c3a7ccc35c1
Zarlex <alex.zarges@online.de>
2017-01-08 00:09:29 +0100

    organize imports

--- app/modules/dashboard/dashboard.module.ts
+++ app/modules/dashboard/dashboard.module.ts
@@ -1,14 +1,11 @@
 import {NgModule}      from '@angular/core';
-
 import {DashboardIndexComponent}     from './components/index/index.component';
-
 import {TracksModule} from '../tracks/tracks.module';
-
 import {DashboardRoutingModule}     from './dashboard.routes';
 import {BrowserModule} from '@angular/platform-browser';
 import {FormsModule} from '@angular/forms';
-import {AudioPlayerModule} from '../audioplayer/audio-player.module';
 import {SessionModule} from '../session/session.module';
+import {BackboneModule} from '../backbone/backbone.module';
 
 @NgModule({
   imports: [
