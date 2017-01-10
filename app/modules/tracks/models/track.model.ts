5443aab8f6b494bb947d97574802246b0a761506
Zarlex <alex.zarges@online.de>
2017-01-11 00:00:21 +0100

    cleanup methods that should not be in track model

--- app/modules/tracks/models/track.model.ts
+++ app/modules/tracks/models/track.model.ts
@@ -9,16 +9,6 @@ import {PlayQueueItem} from '../../audioplayer/models/play_queue_item.model';
 export class Track extends SoundcloudModel {
   endpoint = '/tracks';
 
-  private setIsUserFavourited(): void {
-    if (this.get('id') && Session.getInstance().get('user').get('likes').length > 0) {
-      if (Session.getInstance().get('user').get('likes').findWhere({id: this.id})) {
-       this.set('user_favorite', true);
-      } else {
-        this.set('user_favorite', false);
-      }
-    }
-  }
-
   nested() {
     return {
       user: User,
@@ -28,41 +18,5 @@ export class Track extends SoundcloudModel {
 
   getResourceUrl(): string {
     return `${this.get('stream_url')}?client_id=${this.clientId}`;
-  }
-
-  isPlaying(): boolean {
-    let playingItem = PlayQueue.getInstance().getPlayingItem();
-    return (playingItem && playingItem.get('track').get('id') === this.get('id'));
-  }
-
-  play(): void {
-    PlayQueue.getInstance().addAndPlay({track: this.toJSON()});
-  }
-
-  pause(): void {
-    if (this.isPlaying()) {
-      PlayQueue.getInstance().getPlayingItem().pause();
-    }
-  }
-
-  queue(): void {
-    PlayQueue.getInstance().queue({track: this.toJSON()});
-  }
-
-  isQueued(): boolean {
-    let queuedItems = PlayQueue.getInstance().getQueuedItems();
-    if (queuedItems && queuedItems.find((item: PlayQueueItem) => {
-        return item.get('track').get('id') === this.id;
-      })) {
-      return true;
-    } else {
-      return false;
-    }
-  }
-
-  initialize() {
-    Session.getInstance().get('user').get('likes').on('add remove reset', this.setIsUserFavourited, this);
-    this.on('change:id', this.setIsUserFavourited, this);
-    this.setIsUserFavourited();
   }
 }
