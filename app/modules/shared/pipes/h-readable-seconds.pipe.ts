a1d6d4609b1dd535e1adc57f90370051aa20d1bb
Zarlex <alex.zarges@online.de>
2017-01-10 23:55:34 +0100

    add pipe to display seconds in a human readable format

---
+++ app/modules/shared/pipes/h-readable-seconds.pipe.ts
@@ -0,0 +1,25 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({name: 'hReadableSeconds'})
+export class HumanReadableSecondsPipe implements PipeTransform {
+
+  private formatToHHMMSS(input: number): string {
+    let time = new Date(null);
+    time.setSeconds(input);
+
+    // format time from hh:mm:ss to mm:ss when hh is 0
+    if (time.getHours() === 1) {
+      return time.toISOString().substr(14, 5);
+    } else {
+      return time.toISOString().substr(11, 8);
+    }
+  }
+
+  transform(value: string, args: string[]): any {
+    if (!value){
+      return value;
+    } else {
+      return this.formatToHHMMSS(parseInt(value,10));
+    }
+  }
+}
