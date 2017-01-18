32dc8b16a6d19f6cdb48c5d313a32067530f6f3b
Zarlex <alex.zarges@online.de>
2017-01-18 20:48:06 +0100

    add new route for user playlists

--- app/modules/session/session.routes.ts
+++ app/modules/session/session.routes.ts
@@ -1,13 +1,17 @@
 import {NgModule}             from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
-
 import {SoundcloudCallbackComponent}   from './components/soundcloud-callback/soundcloud_callback.component';
 import {LikedTracksViewComponent} from './components/liked-tracks-view/liked-tracks-view.component';
 import {UserPlayListViewComponent} from './components/user-playlist-view/user-playlist-view.component';
+import {
+  AuthenticatedUserPlaylistsViewComponent
+}
+  from './components/authenticated-user-playlists-view/authenticated-user-playlists-view.component';
 
 const routes: Routes = [
   {path: 'connect', component: SoundcloudCallbackComponent},
   {path: 'me/likes', component: LikedTracksViewComponent},
+  {path: 'me/playlists', component: AuthenticatedUserPlaylistsViewComponent},
   {path: 'me/playlists/:id', component: UserPlayListViewComponent}
 ];
 @NgModule({
