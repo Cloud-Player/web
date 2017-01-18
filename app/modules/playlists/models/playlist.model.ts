d7e129d0e4bff143f6cf997fecf63557aec328bc
Zarlex <alex.zarges@online.de>
2017-01-18 19:50:11 +0100

    add nested cover_url imagmodel and set isPublic attribute in parse

--- app/modules/playlists/models/playlist.model.ts
+++ app/modules/playlists/models/playlist.model.ts
@@ -1,8 +1,9 @@
-
-import {Injectable} from '@angular/core';import {User} from '../../users/models/user.model';
+import {Injectable} from '@angular/core';
+import {User} from '../../users/models/user.model';
 import {Tracks} from '../../tracks/collections/tracks.collection';
 import {map} from 'underscore';
 import {SoundcloudModel} from '../../shared/models/soundcloud.model';
+import {SoundcloudImageModel} from '../../shared/models/soundcloud-image.model';
 
 @Injectable()
 export class Playlist extends SoundcloudModel {
@@ -18,20 +19,35 @@ export class Playlist extends SoundcloudModel {
   nested() {
     return {
       user: User,
+      artwork_url: SoundcloudImageModel,
       tracks: Tracks
     };
   };
 
-  compose(args: any) {
+  parse(attrs: any) {
+    if (attrs.sharing === 'public') {
+      attrs.isPublic = true;
+    } else {
+      attrs.isPublic = false;
+    }
+    delete attrs.sharing;
+
+    if (!attrs.artwork_url && attrs.tracks.length > 0) {
+      attrs.artwork_url = attrs.tracks[0].artwork_url;
+    }
+
+    return attrs;
+  }
+
+  compose(attrs: any) {
     return {
       playlist: {
-        title: args.title,
-        sharing: args.isPublic ? 'public' : 'private',
+        title: attrs.title,
+        sharing: attrs.isPublic ? 'public' : 'private',
         tracks: map(this.get('tracks').toJSON(), (obj: any) => {
           return {id: obj.id};
         })
       }
     };
   }
-
 }
