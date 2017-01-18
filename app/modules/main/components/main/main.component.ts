e9abb8089b506496fe9fe075c8f27e596627eb47
Kai Schulz <mail@kaischulz.com>
2017-01-18 23:03:44 +0100

    remove styleUrls: ['/main.style.css'] from main.components

--- app/modules/main/components/main/main.component.ts
+++ app/modules/main/components/main/main.component.ts
@@ -3,8 +3,7 @@ import './main.style.scss';
 
 @Component({
     selector: 'cloud-player',
-    template: require('./main.template.html'),
-    styleUrls: ['/main.style.css']
+    template: require('./main.template.html')
 })
 
 export class MainComponent {
