6b12864a7c00989ab0a5e77ec16f8b4dec6f616b
Kai Schulz <mail@kaischulz.com>
2017-01-18 17:24:03 +0100

    add root path app and  --display-error-details to web pack

--- config/helpers.js
+++ config/helpers.js
@@ -1,5 +1,5 @@
 var path = require('path');
-var _root = path.resolve(__dirname, '..');
+var _root = path.resolve(__dirname, 'app');
 function root(args) {
   args = Array.prototype.slice.call(arguments, 0);
   return path.join.apply(path, [_root].concat(args));
