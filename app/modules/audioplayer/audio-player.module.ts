26a336f6d08fb858187e30b9841653d2cbf83f3a
Zarlex <alex.zarges@online.de>
2017-01-22 21:05:33 +0100

    insert saved tracks into playqueue on application startup

--- app/modules/audioplayer/audio-player.module.ts
+++ app/modules/audioplayer/audio-player.module.ts
@@ -5,9 +5,11 @@ import {PlayQueueComponent} from './components/playqueue/playqueue.component';
 import {SharedModule} from '../shared/shared.module';
 import {AudioPlayerComponent} from './components/audio-player/audio-player.component';
 import {AudioPlayerControlsComponent} from './components/audio-player-controls/audio-player-controls.component';
-import {debounce} from 'underscore';
+import {debounce, map} from 'underscore';
 import * as localforage from 'localforage';
 import {PlayQueue} from './collections/play_queue.collection';
+import {Tracks} from '../tracks/collections/tracks.collection';
+import {Track} from '../tracks/models/track.model';
 
 @NgModule({
   imports: [
@@ -32,14 +34,25 @@ export class AudioPlayerModule {
 
     localforage.getItem('sc_playqueue').then((playQueueItems: any) => {
       if (playQueueItems) {
-        playQueue.add(playQueueItems);
-        playQueue.fetchTrackInformationOfAllAddedTracks();
+        playQueue.add(playQueueItems, {silent: true});
+        let playQueueItemTracks = map(playQueueItems, (item: any) => {
+          return {
+            id: item.id
+          };
+        });
+        let tmpTracks = new Tracks();
+        tmpTracks.add(playQueueItemTracks);
+        tmpTracks.refresh().then(function (tracks) {
+          tracks.forEach((track: Track) => {
+            playQueue.add({track: track}, {merge: true, silent: true});
+          });
+        });
       }
     });
 
     let debouncedPlayQueueSave = debounce(() => {
       localforage.setItem('sc_playqueue', playQueue.getScheduledItemsJSON(30));
-    }, 100);
+    }, 1000);
     playQueue.on('add remove reset change:status', debouncedPlayQueueSave);
   }
 
