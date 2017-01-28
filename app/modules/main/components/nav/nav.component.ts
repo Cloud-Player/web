0f76a7e0f875e3e0f111169a90e1893b89970489
Daniel Reißner <mail@danielreissner.com>
2017-01-28 14:06:24 +0100

    Add some styles to nav component

--- app/modules/main/components/nav/nav.component.ts
+++ app/modules/main/components/nav/nav.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, trigger, state, style, transition, animate} from '@angular/core';
 import {User} from '../../../users/models/user.model';
 import {Session} from '../../../session/models/session.model';
 
@@ -6,13 +6,31 @@ import {Session} from '../../../session/models/session.model';
   moduleId: module.id,
   selector: 'nav-sidebar',
   templateUrl: 'nav.template.html',
-  styleUrls: ['nav.style.css']
+  styleUrls: ['nav.style.css'],
+  animations: [
+    trigger('toggle', [
+      state('in', style({
+        width: '125px'
+      })),
+      state('out', style({
+        width: '250px'
+      })),
+      transition('in => out', animate('200ms ease-in-out')),
+      transition('out => in', animate('200ms ease-in-out'))
+    ]),
+  ]
 })
 
 export class NavComponent {
+
   private user: User;
+  private toggleState: string = 'in';
 
   constructor() {
     this.user = Session.getInstance().get('user');
+  }
+
+  toggle() {
+    this.toggleState = this.toggleState === 'out' ? 'in' : 'out';
   }
 }
