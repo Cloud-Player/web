a92dee29f366af1796fe27863a989b66115b179f
Daniel Reißner <mail@danielreissner.com>
2017-01-28 14:02:49 +0100

    Add current track to audio player for track information

--- app/modules/audioplayer/components/audio-player/audio-player.component.ts
+++ app/modules/audioplayer/components/audio-player/audio-player.component.ts
@@ -1,7 +1,5 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Input} from '@angular/core';
 import {Track} from '../../../tracks/models/track.model';
-import {PlayQueue} from '../../collections/play_queue.collection';
-import {PlayQueueItem} from '../../models/play_queue_item.model';
 
 @Component ({
   moduleId: module.id,
@@ -10,24 +8,8 @@ import {PlayQueueItem} from '../../models/play_queue_item.model';
   styleUrls: ['audio-player.style.css']
 })
 
-export class AudioPlayerComponent implements OnInit {
+export class AudioPlayerComponent{
 
-  private playQueue: PlayQueue<PlayQueueItem>;
-  private item: PlayQueueItem;
-  private track: Track;
-
-  constructor() {
-  }
-
-  ngOnInit(): void {
-    this.playQueue = PlayQueue.getInstance();
-    this.item = this.playQueue.getCurrentItem();
-    this.playQueue.on('change:status', () => {
-      if (this.playQueue.hasCurrentItem()) {
-        this.item = this.playQueue.getCurrentItem();
-        this.track = this.item.get('track');
-      }
-    });
-  }
+  @Input() track: Track;
 
 }
