9367691bde48fc93a2e5940665f8f5d25b95748a
Daniel Reißner <mail@danielreissner.com>
2017-01-31 16:50:14 +0100

    Extract auth to auth service

---
+++ app/modules/shared/services/auth.service.ts
@@ -0,0 +1,66 @@
+import {Injectable} from '@angular/core';
+import {Session} from '../../session/models/session.model';
+import {User} from '../../users/models/user.model';
+import Timer = NodeJS.Timer;
+
+@Injectable()
+export class AuthService {
+
+  get isAuthenticated(): boolean {
+    return this._isAuthenticated;
+  }
+
+  private session = Session.getInstance();
+  private _isAuthenticated = false;
+  private checkInterval: Timer;
+
+  private receiveConnectMessage(event: any): void {
+    let origin = event.origin || event.originalEvent.origin;
+    if (origin !== 'http://sc.menu-flow.com') {
+      return;
+    }
+    this.connectionSuccessFul(event.data);
+  }
+
+  getConnectionUrl(): string {
+    return '//soundcloud.com/connect?' +
+      'client_id=abb6c1cad3f409112a5995bf922e1d1e&' +
+      'redirect_uri=http://sc.menu-flow.com/connect&' +
+      'response_type=code';
+  }
+
+  connect() {
+    let popup = window.open(this.getConnectionUrl());
+    this.checkInterval = setInterval(() => {
+      popup.postMessage(null, 'http://sc.menu-flow.com');
+    }, 100);
+  }
+
+  disconnect() {
+    this.session.clear();
+  }
+
+  connectionSuccessFul(params: any) {
+    this.session.set({
+      access_token: params.access_token,
+      expires_on: params.expires_on,
+      refresh_token: params.refresh_token
+    });
+  }
+
+  constructor() {
+    window.addEventListener('message', this.receiveConnectMessage.bind(this), false);
+    window.addEventListener('connectionSuccessFul', (ev: CustomEvent) => {
+      let creds = ev.detail;
+      if (creds) {
+        try {
+          creds = JSON.parse(creds);
+        } catch (err) {
+
+        }
+        this.connectionSuccessFul(creds);
+      }
+    });
+  }
+
+}
