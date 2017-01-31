c226166e0cd2d7ac68a38cde5c33c10ad586910e
Daniel Reißner <mail@danielreissner.com>
2017-01-31 16:36:06 +0100

    Modify style of main component

--- app/modules/main/components/main/main.component.ts
+++ app/modules/main/components/main/main.component.ts
@@ -2,6 +2,8 @@ import {Component, OnInit} from '@angular/core';
 import {PlayQueueItem} from '../../../audioplayer/models/play_queue_item.model';
 import {PlayQueue} from '../../../audioplayer/collections/play_queue.collection';
 import {Track} from '../../../tracks/models/track.model';
+import {User} from '../../../users/models/user.model';
+import {Session} from '../../../session/models/session.model';
 
 @Component({
   selector: 'cloud-player',
@@ -12,10 +14,13 @@ import {Track} from '../../../tracks/models/track.model';
 export class MainComponent implements OnInit {
 
   private playQueue: PlayQueue<PlayQueueItem>;
+  private isAuthenticated: boolean = false;
   private item: PlayQueueItem;
+  private session: Session;
   private track: Track;
 
   constructor() {
+    this.session = Session.getInstance();
   }
 
   ngOnInit(): void {
@@ -27,5 +32,23 @@ export class MainComponent implements OnInit {
         this.track = this.item.get('track');
       }
     });
+    this.session.get('user').on('change:authenticated', (user: User) => {
+      this.setAuthenticated(user);
+    });
+
+    if (this.session.isValid()) {
+      this.setAuthenticated(this.session.get('user'));
+    }
   }
+
+  private setAuthenticated(user: User) {
+    if (user.get('authenticated')) {
+      user.fetch().then(() => {
+        this.isAuthenticated = true;
+        user.get('likes').fetch();
+      });
+    } else {
+      this.isAuthenticated = false;
+    }
+  };
 }
