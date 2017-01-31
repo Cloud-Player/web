d05bc389ead27260a4920132c0cd0aa807a26bcd
Daniel Reißner <mail@danielreissner.com>
2017-01-31 16:36:32 +0100

    Modify style of nav component

--- app/modules/main/components/nav/nav.component.ts
+++ app/modules/main/components/nav/nav.component.ts
@@ -1,6 +1,7 @@
 import {Component, trigger, state, style, transition, animate} from '@angular/core';
 import {User} from '../../../users/models/user.model';
 import {Session} from '../../../session/models/session.model';
+import {AuthService} from '../../../shared/services/auth.service';
 
 @Component({
   selector: 'nav-sidebar',
@@ -23,13 +24,47 @@ import {Session} from '../../../session/models/session.model';
 export class NavComponent {
 
   private user: User;
+  private session: Session;
+  private isAuthenticated: boolean;
   private toggleState: string = 'in';
+  private imgUrl: string = 'https://a-v2.sndcdn.com/assets/images/header/cloud@2x-e5fba4.png';
 
-  constructor() {
-    this.user = Session.getInstance().get('user');
+  constructor(private authService: AuthService) {
+    this.session = Session.getInstance();
+    this.user = this.session.get('user');
   }
 
   toggle() {
     this.toggleState = this.toggleState === 'out' ? 'in' : 'out';
   }
+
+  connect() {
+    this.authService.connect();
+  }
+
+  disconnect() {
+    this.authService.disconnect();
+  }
+
+
+  private setAuthenticated(user: User) {
+    if (user.get('authenticated')) {
+      user.fetch().then(() => {
+        this.isAuthenticated = true;
+        user.get('likes').fetch();
+      });
+    } else {
+      this.isAuthenticated = false;
+    }
+  };
+
+  ngOnInit(): void {
+    this.session.get('user').on('change:authenticated', (user: User) => {
+      this.setAuthenticated(user);
+    });
+
+    if (this.session.isValid()) {
+      this.setAuthenticated(this.session.get('user'));
+    }
+  };
 }
