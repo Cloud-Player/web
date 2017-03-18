d9f2d31088ef00bf94197ef8df96d5b18cc7a458
Zarlex <alex.zarges@online.de>
2017-03-18 16:09:39 +0100

    add view change loader component

---
+++ app/modules/shared/components/view-change-loader/view-change-loader.component.ts
@@ -0,0 +1,58 @@
+import {Component, OnInit, EventEmitter, Output, NgZone, Renderer, ElementRef} from '@angular/core';
+import {Router, NavigationStart, NavigationEnd, NavigationError, NavigationCancel} from '@angular/router';
+
+@Component({
+  selector: 'view-change-loader',
+  styles: [require('./view-change-loader.style.scss')],
+  template: require('./view-change-loader.template.html'),
+})
+
+export class ViewChangeLoaderComponent {
+
+  constructor(private el: ElementRef,
+              private router: Router,
+              private ngZone: NgZone,
+              private renderer: Renderer) {
+    router.events.subscribe((event: any) => {
+      this.navigationInterceptor(event);
+    });
+  }
+
+  private navigationInterceptor(event: any): void {
+
+    if (event instanceof NavigationStart) {
+      this.showSpinner();
+    }
+    if (event instanceof NavigationEnd
+      || event instanceof NavigationCancel
+      || event instanceof NavigationError
+    ) {
+      this.hideSpinner();
+    }
+  }
+
+  private showSpinner(): void {
+    // We wanna run this function outside of Angular's zone to
+    // bypass change detection
+    this.ngZone.runOutsideAngular(() => {
+      this.renderer.setElementStyle(
+        this.el.nativeElement,
+        'display',
+        'block'
+      );
+    });
+  }
+
+  private hideSpinner(): void {
+
+    // We wanna run this function outside of Angular's zone to
+    // bypass change detection,
+    this.ngZone.runOutsideAngular(() => {
+      this.renderer.setElementStyle(
+        this.el.nativeElement,
+        'display',
+        'none'
+      );
+    });
+  }
+}
