130686aa4601610be935f85247f3b543da125488
Zarlex <alex.zarges@online.de>
2017-03-18 23:04:40 +0100

    trigger initial search and save search value

--- app/modules/dashboard/components/index/index.component.ts
+++ app/modules/dashboard/components/index/index.component.ts
@@ -1,22 +1,35 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, ViewChild, ElementRef, AfterViewInit} from '@angular/core';
 
 import {Tracks} from '../../../tracks/collections/tracks.collection';
 import {Track} from '../../../tracks/models/track.model';
+import {CollectionTextInputSearchComponent} from '../../../shared/components/collection-text-input-search/collection-text-input-search.component';
+import localforage = require('localforage');
 
 @Component({
   selector: 'my-dashboard',
   styles: [ require('./index.style.scss') ],
-  template: require('./index.template.html'),
-  providers: [Tracks]
+  template: require('./index.template.html')
 })
 
-export class DashboardIndexComponent implements OnInit {
+export class DashboardIndexComponent implements AfterViewInit {
   title = 'Search Tracks';
+
+  @ViewChild('searchBar') searchBar: CollectionTextInputSearchComponent;
 
   constructor(private tracks: Tracks<Track>) {
   }
 
-  ngOnInit(): void {
-    this.tracks.fetch();
+  ngAfterViewInit(){
+    this.searchBar.focus();
+
+    localforage.getItem('sc_search_term').then((val: string) => {
+      if(val){
+        this.searchBar.search(val);
+      }
+    });
+
+    this.searchBar.valueChange.subscribe((val: string)=>{
+      localforage.setItem('sc_search_term', val);
+    });
   }
 }
