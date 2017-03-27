f2473cd813ae1ff80779ca4996ef22361d9c4d18
Zarlex <alex.zarges@online.de>
2017-03-27 20:43:16 +0200

    uncomment stuff

---
+++ app/modules/dashboard/collections/search-filters.collection.ts
@@ -0,0 +1,9 @@
+// import {Injectable} from '@angular/core';
+// import {SearchFilter} from '../models/search-filter.model';
+// import {LocalCollection} from '../../backbone/collections/local.collection';
+//
+// @Injectable()
+// export class SearchFilters <TModel extends SearchFilter> extends LocalCollection<SearchFilter> {
+//   model: any = SearchFilter;
+//   localStoreId: string = 'sc_search_filters';
+// }
---
+++ app/modules/dashboard/models/search-filter.model.ts
@@ -0,0 +1,8 @@
+// import {Injectable} from '@angular/core';
+// import localforage = require('localforage');
+// import {LocalModel} from '../../backbone/models/local.model';
+//
+// @Injectable()
+// export class SearchFilter extends LocalModel {
+//   localStoreId: string = 'sc_search_filters';
+// }
