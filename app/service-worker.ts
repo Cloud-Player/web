8aefd250750893bf2e0192ff584352635bef87ab
Zarlex <alex.zarges@online.de>
2017-04-06 22:50:11 +0200

    limit service worker to own domain

--- app/service-worker.ts
+++ app/service-worker.ts
@@ -4,29 +4,32 @@ var g: any = global;
 var cacheVersion: string = VERSION;
 
 self.addEventListener('install', function (event: any) {
+  self.skipWaiting()
   event.waitUntil(
     self.caches.open(cacheVersion).then(function (cache) {
       return cache.addAll(g.serviceWorkerOption.assets);
     })
   );
-  event.waitUntil(self.skipWaiting());
 });
 
 self.addEventListener('fetch', function (event: any) {
-  event.respondWith(
-    self.caches.match(event.request)
-      .then(
-        function (response) {
-          if (response) {
-            return response;
+  var url = new URL(event.request.url);
+  if (url.origin === location.origin) {
+    event.respondWith(
+      self.caches.match(event.request)
+        .then(
+          function (response) {
+            if (response) {
+              return response;
+            }
+            return fetch(event.request);
+          },
+          function () {
+            // Offline Fallback
           }
-          return fetch(event.request);
-        },
-        function () {
-          // Offline Fallback
-        }
-      )
-  );
+        )
+    );
+  }
 });
 
 self.addEventListener('activate', function (event: any) {
