c269c70434785c46fb7cf1063efaf4e9bddfea64
Zarlex <alex.zarges@online.de>
2017-04-06 20:33:06 +0200

    Release new Version

---
+++ sw.js
@@ -0,0 +1,95 @@
+var serviceWorkerOption = {
+  "assets": [
+    "/ebd7bf47dcdbd9ebebea93f422e84dd1.svg",
+    "/674f50d287a8c48dc19ba404d20fe713.eot",
+    "/af7ae505a9eed503f8b8e6982036873e.woff2",
+    "/fee66e712a8a08eef5805a46892932ad.woff",
+    "/b06871f281fee6b241d60582ae9369b9.ttf",
+    "/912ec66d7572ff821749319396470bde.svg",
+    "/app.3152ee2bbf919181808d.js",
+    "/polyfills.3152ee2bbf919181808d.js",
+    "/vendor.3152ee2bbf919181808d.js"
+  ]
+};
+        
+        /******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId])
+/******/ 			return installedModules[moduleId].exports;
+
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			exports: {},
+/******/ 			id: moduleId,
+/******/ 			loaded: false
+/******/ 		};
+
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+
+/******/ 		// Flag the module as loaded
+/******/ 		module.loaded = true;
+
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+
+
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ function(module, exports, __webpack_require__) {
+
+	/* WEBPACK VAR INJECTION */(function(global) {"use strict";
+	var g = global;
+	var cacheVersion = ("0.5.1");
+	self.addEventListener('install', function (event) {
+	    event.waitUntil(self.caches.open(cacheVersion).then(function (cache) {
+	        return cache.addAll(g.serviceWorkerOption.assets);
+	    }));
+	    event.waitUntil(self.skipWaiting());
+	});
+	self.addEventListener('fetch', function (event) {
+	    event.respondWith(self.caches.match(event.request)
+	        .then(function (response) {
+	        if (response) {
+	            return response;
+	        }
+	        return fetch(event.request);
+	    }, function () {
+	        // Offline Fallback
+	    }));
+	});
+	self.addEventListener('activate', function (event) {
+	    event.waitUntil(self.caches.keys().then(function (keyList) {
+	        return Promise.all(keyList.map(function (key) {
+	            if (key !== cacheVersion) {
+	                return self.caches.delete(key);
+	            }
+	        }));
+	    }));
+	    event.waitUntil(self.clients.claim());
+	});
+
+	/* WEBPACK VAR INJECTION */}.call(exports, (function() { return this; }())))
+
+/***/ }
+/******/ ]);
