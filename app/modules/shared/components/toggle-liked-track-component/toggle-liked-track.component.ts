a729dcc2d02384e4f3137ea883f28b707c83d19e
Zarlex <alex.zarges@online.de>
2017-04-06 16:38:13 +0200

    check if user is authenticated and show tooltip to connect if not

--- app/modules/shared/components/toggle-liked-track-component/toggle-liked-track.component.ts
+++ app/modules/shared/components/toggle-liked-track-component/toggle-liked-track.component.ts
@@ -1,15 +1,37 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, trigger, state, transition, style, animate} from '@angular/core';
 import {Track} from '../../../tracks/models/track.model';
 import {Session} from '../../../session/models/session.model';
+import {AuthService} from '../../services/auth.service';
 
 @Component({
   selector: 'toggle-liked-track',
-  styles: [ require('./toggle-liked-track.style.scss') ],
-  template: require('./toggle-liked-track.template.html')
+  styles: [require('./toggle-liked-track.style.scss')],
+  template: require('./toggle-liked-track.template.html'),
+  animations: [
+    trigger('visibilityChanged', [
+      state('true', style({width: '*', opacity: 1})),
+      state('false', style({width: 0, display: 'none', opacity: 0})),
+      state('void', style({width: 0, display: 'none', opacity: 0})),
+      transition('* => *', animate('300ms ease-in-out'))
+    ])
+  ]
 })
 export class ToggleLikedTrackComponent {
+  private session = Session.getInstance();
 
   @Input() track: Track;
+
+  public showAuthenticateTooltip: boolean = false;
+
+  constructor(private authService: AuthService) {
+  }
+
+  showConnectTooltip() {
+    this.showAuthenticateTooltip = true;
+    setTimeout(()=>{
+      this.showAuthenticateTooltip = false;
+    }, 2000)
+  }
 
   hasLikedTrack(): boolean {
     if (this.track && this.track.get('id') && Session.getInstance().get('user').get('likes').length > 0) {
@@ -17,8 +39,16 @@ export class ToggleLikedTrackComponent {
     }
   }
 
+  connect() {
+    this.authService.connect();
+  }
+
   like(): void {
-    Session.getInstance().get('user').get('likes').create(this.track.toJSON());
+    if (this.session.isValid()) {
+      Session.getInstance().get('user').get('likes').create(this.track.toJSON());
+    } else {
+      this.showConnectTooltip();
+    }
   }
 
   dislike(): void {
