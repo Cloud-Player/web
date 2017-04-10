8f07afecdb41f316e682ce682df1ea53a721c57c
Zarlex <alex.zarges@online.de>
2017-04-10 16:01:27 +0200

    add caching of google font

--- app/service-worker.ts
+++ app/service-worker.ts
@@ -2,9 +2,13 @@ declare const VERSION: string;
 
 var g: any = global;
 var cacheVersion: string = VERSION;
+var externalDomainWhiteList = [
+  'https://fonts.gstatic.com',
+  'https://fonts.googleapis.com'
+];
 
 self.addEventListener('install', function (event: any) {
-  self.skipWaiting()
+  self.skipWaiting();
   event.waitUntil(
     self.caches.open(cacheVersion).then(function (cache) {
       return cache.addAll(g.serviceWorkerOption.assets);
@@ -14,7 +18,7 @@ self.addEventListener('install', function (event: any) {
 
 self.addEventListener('fetch', function (event: any) {
   var url = new URL(event.request.url);
-  if (url.origin === location.origin) {
+  if (url.origin === location.origin || externalDomainWhiteList.indexOf(url.origin) !== -1) {
     event.respondWith(
       self.caches.match(event.request)
         .then(
@@ -22,7 +26,24 @@ self.addEventListener('fetch', function (event: any) {
             if (response) {
               return response;
             }
-            return fetch(event.request);
+            var fetchRequest = event.request.clone();
+
+            return fetch(fetchRequest).then(
+              function (response) {
+                if (!response || response.status !== 200 || response.type !== 'basic') {
+                  return response;
+                }
+
+                var responseToCache = response.clone();
+
+                self.caches.open(cacheVersion)
+                  .then(function (cache) {
+                    cache.put(event.request, responseToCache);
+                  });
+
+                return response;
+              }
+            );
           },
           function () {
             // Offline Fallback
