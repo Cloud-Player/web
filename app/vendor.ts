a0bbe3ba541e3d083ed17456761ee4331c67a5e6
Zarlex <alex.zarges@online.de>
2017-04-10 16:01:36 +0200

    remove jquery

--- app/vendor.ts
+++ app/vendor.ts
@@ -11,10 +11,8 @@ import 'rxjs';
 
 // You can import js, ts, css, sass, ...
 import 'font-awesome/css/font-awesome.css';
-import './_variables.scss';
 
 // Other vendors for example jQuery, Lodash or Bootstrap
-import 'jquery';
 import 'backbone';
 import 'underscore';
 import 'localforage';
