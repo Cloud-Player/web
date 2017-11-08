8a8be87acb3f49edec13be54801a31f6dc207429
Zarlex <alex.zarges@online.de>
2017-11-08 19:44:26 +0100

    Add sh file to create a zip file from dist folder with current package.json version number as name

---
+++ bin/zip_version.sh
@@ -0,0 +1,22 @@
+#/bin/sh
+set -e
+
+# Get version number from package json version
+VERSION_NUMBER=$(cat package.json \
+  | grep version \
+  | head -1 \
+  | awk -F: '{ print $2 }' \
+  | sed 's/[",]//g' \
+  | xargs)
+
+echo "####################################"
+echo " Zip version ${VERSION_NUMBER}... "
+echo "####################################"
+
+# Create a zip file that is attached to the github release
+mkdir -p zip
+cp -r dist zip/${VERSION_NUMBER}
+cd zip
+zip -r cloud-player-v${VERSION_NUMBER}.zip ${VERSION_NUMBER}
+rm -rf ${VERSION_NUMBER}
+cd ..
