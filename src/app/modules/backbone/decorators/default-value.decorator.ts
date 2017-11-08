e7b79966f15874a9d1f3656df06fb1cc7b5b1dd0
Zarlex <alex.zarges@online.de>
2017-11-08 16:25:53 +0100

    Add default decorator to set a default value

---
+++ src/app/modules/backbone/decorators/default-value.decorator.ts
@@ -0,0 +1,18 @@
+import {result, pairs} from 'underscore';
+import {getAttributesMapKey} from '../utils/get-attributes-map-key';
+
+export function defaultValue(value: any): any {
+  return function (target: any, propertyKey: string): void {
+    const defaults = result(target, 'defaults') || {};
+
+    Object.defineProperty(target, 'defaults', {
+      get: function () {
+        const mappedKey = getAttributesMapKey(this, propertyKey);
+        defaults[mappedKey] = value;
+        return defaults;
+      },
+      set: function () {
+      }
+    });
+  };
+}
