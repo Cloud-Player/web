f5575e01fe01b9df9212cb497f9434b9d99800b1
Zarlex <alex.zarges@online.de>
2017-12-07 16:51:21 +0100

    Move functionality into an abstract player, inherit from it and implement required methods

--- src/app/modules/audioplayer/components/soundcloud-player/soundcloud-player.ts
+++ src/app/modules/audioplayer/components/soundcloud-player/soundcloud-player.ts
@@ -1,264 +1,106 @@
-import {Component, EventEmitter, Input, OnDestroy, OnInit, Output, Renderer, Renderer2} from '@angular/core';
+import {
+  Component, ElementRef, OnDestroy, OnInit, Renderer2
+} from '@angular/core';
 import {Track} from '../../../tracks/models/track.model';
-import {PlayerStatus} from '../../enums/player-status';
-import {IPlayer} from '../../interfaces/player';
-import {EaseService} from '../../../shared/services/ease.service';
-import {Observable} from 'rxjs/Observable';
 import {CoverSizes} from '../../../shared/components/track-cover/track-cover.component';
+import {IPlayer} from '../../src/player.interface';
+import {AbstractPlayer} from '../../src/abstract-player.class';
 
 @Component({
   selector: 'app-soundcloud-player',
   styleUrls: ['./soundcloud-player.scss'],
   templateUrl: './soundcloud-player.html'
 })
-export class SoundcloudPlayerComponent implements IPlayer, OnDestroy, OnInit {
-  private _initialised = false;
-  private _duration: number;
-  private _currentTime: number;
-  private _status: PlayerStatus;
-  private _audio: HTMLAudioElement;
+export class SoundcloudPlayerComponent extends AbstractPlayer implements IPlayer, OnDestroy {
   private _listeners: Function[] = [];
-  private _volume: number;
-  private _initialisePlayerPromise;
-  private _initialisePromise;
+  private _audio: HTMLAudioElement;
 
-  constructor(private renderer: Renderer2) {
-    this._audio = new Audio();
-    this.setStatus(PlayerStatus.NotInitialised);
+  constructor(private renderer: Renderer2, private el: ElementRef) {
+    super();
   }
 
-  public canPlay: boolean;
-
-  @Input()
-  public track: Track;
-
-  @Output()
-  public durationChange = new EventEmitter();
-
-  @Output()
-  public currentTimeChange = new EventEmitter();
-
-  @Output()
-  public statusChange = new EventEmitter();
-
-  private setDuration(duration: number) {
-    this._duration = duration;
-    this.durationChange.emit(duration);
-  }
-
-  private setCurrentTime(currentTime: number): void {
-    this._currentTime = currentTime;
-    this.currentTimeChange.emit(currentTime);
-  }
-
-  private setStatus(status: PlayerStatus) {
-    this._status = status;
-    this.statusChange.emit(status);
-  }
-
-  public bindListeners() {
+  protected bindListeners() {
     this.unBindListeners();
     this._listeners = [
-      this.renderer.listen(this._audio, 'canplay', () => {
-        this.setStatus(PlayerStatus.Ready);
-        this.canPlay = true;
-      }),
+      this.renderer.listen(this._audio, 'canplay', this.onIsAbleToPlay.bind(this)),
+      this.renderer.listen(this._audio, 'loadStart', this.onWaiting.bind(this)),
+      this.renderer.listen(this._audio, 'waiting', this.onWaiting.bind(this)),
+      this.renderer.listen(this._audio, 'playing', this.onPlaying.bind(this)),
+      this.renderer.listen(this._audio, 'pause', this.onPaused.bind(this)),
+      this.renderer.listen(this._audio, 'ended', this.onEnded.bind(this)),
+      this.renderer.listen(this._audio, 'error', this.onError.bind(this)),
       this.renderer.listen(this._audio, 'durationchange', () => {
-        this.setDuration(this._audio.duration);
+        this.onDurationUpdate(this._audio.duration);
       }),
       this.renderer.listen(this._audio, 'timeupdate', () => {
-        this.setCurrentTime(this._audio.currentTime);
-      }),
-      this.renderer.listen(this._audio, 'loadStart', () => {
-        this.setStatus(PlayerStatus.Waiting);
-      }),
-      this.renderer.listen(this._audio, 'waiting', () => {
-        this.setStatus(PlayerStatus.Waiting);
-      }),
-      this.renderer.listen(this._audio, 'playing', () => {
-        this.setStatus(PlayerStatus.Playing);
-      }),
-      this.renderer.listen(this._audio, 'ended', () => {
-        this.setStatus(PlayerStatus.Ended);
-      }),
-      this.renderer.listen(this._audio, 'error', () => {
-        this.setStatus(PlayerStatus.Error);
+        this.onCurrentTimeUpdate(this._audio.currentTime);
       })
     ];
   }
 
-  public unBindListeners() {
+  protected unBindListeners() {
     this._listeners.forEach((unbindListener: Function) => {
       unbindListener();
     });
   }
 
-  public get volume(): number {
-    return this._volume;
-  }
-
-  public set volume(volume: number) {
-    this._volume = volume;
-    this._audio.volume = volume;
-  }
-
-  public get duration(): number {
-    return this._duration;
-  }
-
-  public get currentTime(): number {
-    return this._currentTime;
-  }
-
-  public get status(): PlayerStatus {
-    return this._status;
-  }
-
-  public get isInitialised(): boolean {
-    return this._initialised;
-  }
-
-  public initialisePlayerSDK(): Promise<any> {
+  protected initialisePlayerSDK(): Promise<any> {
     return new Promise((resolve) => {
       resolve();
     });
   }
 
-  public initialisePlayer(): Promise<boolean> {
-    if (!this._initialisePlayerPromise) {
-      this._initialisePlayerPromise =
-        new Promise((resolve) => {
-          if (this._initialised) {
-            resolve(true);
-            this._initialisePlayerPromise = null;
-          } else {
-            this._initialised = true;
-            this._audio.src = this.track.getResourceUrl();
-            resolve(true);
-            this._initialisePlayerPromise = null;
-          }
-        });
-    }
-
-    return this._initialisePlayerPromise;
-  }
-
-  public preload() {
-    this._audio.load();
-  }
-
-  public initialise(): Promise<any> {
-    if (!this._initialisePromise) {
-      this._initialisePromise = new Promise(resolve => {
-        if (this._initialised) {
-          resolve();
-          this._initialisePromise = null;
-        } else {
-          this._initialisePromise =
-            this.initialisePlayerSDK().then(() => {
-              this.initialisePlayer().then(() => {
-                this._initialised = true;
-                this.volume = this.volume;
-                this.preload();
-                this.pause();
-                this.bindListeners();
-                this.setStatus(PlayerStatus.Idle);
-                resolve();
-                this._initialisePromise = null;
-              });
-            });
-        }
-      });
-    }
-
-    return this._initialisePromise;
-  }
-
-  public seekTo(to: number) {
-    this._audio.currentTime = to;
-    this._audio.play();
-  }
-
-  public play(from: number = 0): void {
-    this.initialise().then(() => {
-      if (from) {
-        this.seekTo(from);
-      } else {
-        this._audio.play();
-      }
+  protected initialisePlayer(): Promise<boolean> {
+    return new Promise((resolve) => {
+      this._audio = new Audio();
+      this._audio.src = this.track.getResourceUrl();
+      this.track.comments.fetch();
+      resolve(true);
     });
   }
 
-  public pause(): void {
-    this._audio.pause();
-    this.setStatus(PlayerStatus.Paused);
+  protected deInitialisePlayer(): void {
+    this._audio.src = '';
+    this.track.comments.reset();
+    this.setDuration(0);
   }
 
-  public stop(): void {
+  protected setPlayerVolume(volume: number) {
+    this._audio.volume = volume;
+  }
+
+  protected preloadTrack(track: Track) {
+    this._audio.src = track.getResourceUrl();
+    this._audio.load();
+  }
+
+  protected startPlayer(): void {
+    this._audio.play().then(null, () => {
+      console.warn('Audio Element could not be started!');
+      this.setAbleToPlay(false);
+      this.onStopped();
+    });
+  }
+
+  protected pausePlayer(): void {
+    this._audio.pause();
+  }
+
+  protected stopPlayer(): void {
     this._audio.pause();
     this._audio.currentTime = 0;
-    this.setStatus(PlayerStatus.Stopped);
+  }
+
+  protected seekPlayerTo(to: number): void {
+    this._audio.currentTime = to;
   }
 
   public getCoverSize(): CoverSizes {
     return CoverSizes.Large;
   }
 
-  public fadeIn(duration: number): Observable<number> {
-    if (this.status === PlayerStatus.Waiting
-    ) {
-      this.statusChange
-        .filter(event => event === PlayerStatus.Ready)
-        .subscribe(() => {
-          this.fadeIn(duration);
-          this.statusChange.unsubscribe();
-        });
-    } else {
-      const obs: Observable<number> = EaseService.easeInCirc(1, 0, duration);
-      obs.subscribe(newVal => {
-        if (this._audio) {
-          this._audio.volume = this.volume - newVal;
-        }
-      });
-      return obs;
-    }
-  }
-
-  public fadeOut(duration: number): Observable<number> {
-    const obs: Observable<number> = EaseService.easeInCirc(0, 1, duration);
-    obs.subscribe(newVal => {
-      if (this._audio) {
-        this._audio.volume = this.volume - newVal;
-      }
-    });
-    return obs;
-  }
-
-  public updateTrack(track: Track) {
-    if (track && track.id === this.track.id) {
-      return;
-    }
-    this.deInitialize();
-    this.track = track;
-    this.initialise();
-    this.play();
-  }
-
-  public deInitialize(): void {
-    this.setStatus(PlayerStatus.NotInitialised);
-    this.unBindListeners();
-    this.stop();
-    this._audio.src = '';
-    this.track.comments.reset();
-    this._initialisePromise = null;
-    this._initialisePlayerPromise = null;
-    this._initialised = false;
-    this._duration = null;
-  }
-
-  ngOnInit(): void {
-    this.track.comments.fetch();
-    this.initialise();
+  public getPlayerEl(): ElementRef {
+    return this.el;
   }
 
   ngOnDestroy(): void {
