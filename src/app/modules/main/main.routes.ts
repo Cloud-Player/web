684b472874fe116fe8ab268cbb5717c0870aa2a1
Zarlex <alex.zarges@online.de>
2017-12-10 20:58:28 +0100

    Move desktop app component into native-client module

--- src/app/modules/main/main.routes.ts
+++ src/app/modules/main/main.routes.ts
@@ -1,10 +1,8 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
-import {DesktopAppViewComponent} from './components/desktop-app-view/desktop-app-view.component';
 
 const routes: Routes = [
-  {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
-  {path: 'desktop-app', component: DesktopAppViewComponent}
+  {path: '', redirectTo: '/dashboard', pathMatch: 'full'}
 ];
 
 @NgModule({
