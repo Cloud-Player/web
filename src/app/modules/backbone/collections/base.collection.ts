94376a5703e45c8ad2855dbce3ac66f4d6076e1e
Zarlex <alex.zarges@online.de>
2017-12-17 14:25:41 +0100

    Allow dynamic model instances

--- src/app/modules/backbone/collections/base.collection.ts
+++ src/app/modules/backbone/collections/base.collection.ts
@@ -1,17 +1,39 @@
 import {BaseModel} from '../models/base.model';
 import {getUrl} from '../utils/get_url.util';
-import {extend} from 'underscore';
+import {extend, isArray, result} from 'underscore';
 import {prepareParams} from '../utils/prepare_params';
 import {SelectableCollection} from './selectable.collection';
+import {Model} from 'backbone';
+import {IDynamicInstances, IModelConstructor} from '../utils/interfaces';
+import {InstanceResolver} from '../utils/instance-resolver';
+import {request} from '../utils/request.util';
 
 export class BaseCollection<TModel extends BaseModel> extends SelectableCollection<TModel> {
-  model: any = BaseModel;
+  model: IModelConstructor = BaseModel;
 
   queryParams: Object = {};
 
   endpoint: string = null;
 
   sortOrder: string = null;
+
+  private prepareDynamicModel(item: TModel | {}): TModel | {} {
+    if (!(item instanceof this.model)) {
+      const dynamicInstances = result(this, 'dynamicInstances');
+      const instance = InstanceResolver.getDynamicInstance(dynamicInstances, 'model', item);
+
+      if (instance instanceof Model) {
+        instance.set(item);
+        item = instance;
+      }
+    }
+
+    return item;
+  }
+
+  dynamicInstances(): IDynamicInstances {
+    return {};
+  }
 
   hostName(): string {
     return '';
@@ -23,7 +45,7 @@ export class BaseCollection<TModel extends BaseModel> extends SelectableCollecti
 
   url = () => {
     return getUrl(this);
-  }
+  };
 
   sync(method: string, model: any, options: any = {}) {
     let queryParams = this.queryParams;
@@ -75,6 +97,25 @@ export class BaseCollection<TModel extends BaseModel> extends SelectableCollecti
         this.sortAscending();
       }
     }
+  }
+
+  request(url: string, method: string, options?: any) {
+    return request(url, method, options, this);
+  }
+
+  add(item: TModel | TModel[] | {}, options: any = {}): any {
+    if (isArray(item)) {
+      const addedItems: Array<TModel | {}> = [];
+      item.forEach((obj: any) => {
+        addedItems.push(this.prepareDynamicModel(obj));
+      });
+      item = addedItems;
+    } else {
+      item = this.prepareDynamicModel(item);
+    }
+
+    item = super.add(item, options);
+    return item;
   }
 
 }
