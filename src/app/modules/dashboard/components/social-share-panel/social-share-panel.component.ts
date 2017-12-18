2c33d588e52197bb7a01ead4b703322f554815af
Zarlex <alex.zarges@online.de>
2017-12-18 14:25:48 +0100

    Update social share panel to let user follow cp on tw

--- src/app/modules/dashboard/components/social-share-panel/social-share-panel.component.ts
+++ src/app/modules/dashboard/components/social-share-panel/social-share-panel.component.ts
@@ -12,16 +12,16 @@ export class SocialSharePanelComponent implements OnInit {
   constructor(private el: ElementRef, private userAnalyticsService: UserAnalyticsService) {
   }
 
-  public shared(type: string) {
+  public followed(type: string) {
     this.dismiss();
-    this.userAnalyticsService.trackEvent('social_shared_on_' + type, 'click', 'app-social-share-panel-component');
-    localforage.setItem('shared_cp', type);
+    this.userAnalyticsService.trackEvent('follow_on_' + type, 'click', 'app-social-share-panel-component');
+    localforage.setItem('followed_cp', type);
   }
 
   public notShared() {
     this.dismiss();
     this.userAnalyticsService.trackEvent('social_not_shared', 'click', 'app-social-share-panel-component');
-    localforage.setItem('shared_cp', 'none');
+    localforage.setItem('followed_cp', 'none');
   }
 
   public dismiss() {
@@ -30,7 +30,7 @@ export class SocialSharePanelComponent implements OnInit {
 
   ngOnInit(): void {
     this.el.nativeElement.style.display = 'none';
-    localforage.getItem('shared_cp').then((value: string) => {
+    localforage.getItem('followed_cp').then((value: string) => {
       if (!value) {
         this.el.nativeElement.style.display = 'block';
       }
