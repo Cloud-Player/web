052c98897abd9df88049432c3eb27e572192c651
Zarlex <alex.zarges@online.de>
2017-12-23 18:34:23 +0100

    Check whether drag is supported for playlist otherwise show toast

--- src/app/modules/session/components/authenticated-user-playlists/authenticated_user_playlists.ts
+++ src/app/modules/session/components/authenticated-user-playlists/authenticated_user_playlists.ts
@@ -5,6 +5,9 @@ import {UserAnalyticsService} from '../../../user-analytics/services/user-analyt
 import {Playlists} from '../../../playlists/collections/playlists.collection';
 import {Playlist} from '../../../playlists/models/playlist.model';
 import {AuthenticatedUserPlaylistTrack} from '../../models/authenticated_user_playlist_track.model';
+import {ToastModel} from '../../../shared/models/toast';
+import {ToastService} from '../../../shared/services/toast';
+import {ToastTypes} from '../../../shared/src/toast-types.enum';
 
 @Component({
   selector: 'app-authenticated-user-playlists',
@@ -24,12 +27,20 @@ export class AuthenticatedUserPlaylistsComponent implements OnInit {
 
   public tmpPlaylist = new AuthenticatedUserPlaylist();
 
-  public dropTrack = (dropData: {}, playlist: AuthenticatedUserPlaylist): void => {
-    this.userAnalyticsService.trackEvent('drop_track', 'drag-and-drop', 'menu-playlist-bar');
-    playlist.tracks.create(new AuthenticatedUserPlaylistTrack(dropData));
+  public dropTrack = (dropData: any, playlist: AuthenticatedUserPlaylist): void => {
+    if (dropData.provider === 'SOUNDCLOUD') {
+      this.userAnalyticsService.trackEvent('drop_track', 'drag-and-drop', 'menu-playlist-bar');
+      playlist.tracks.create(new AuthenticatedUserPlaylistTrack(dropData));
+    } else {
+      const toast = new ToastModel();
+      toast.type = ToastTypes.Info;
+      toast.message = 'You can not drag a YouTube track into SoundCloud playlist';
+      this.toastService.addToast(toast);
+      this.userAnalyticsService.trackEvent('drop_track_err', 'drag-and-drop', 'menu-playlist-bar');
+    }
   }
 
-  constructor(private userAnalyticsService: UserAnalyticsService) {
+  constructor(private userAnalyticsService: UserAnalyticsService, private toastService: ToastService) {
   }
 
   private fetchPlaylists(): void {
