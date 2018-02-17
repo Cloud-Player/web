6f78c3fc223883c98d206ba4175c243339a7d635
Zarlex <alex.zarges@online.de>
2018-02-17 20:08:26 +0100

    Add empty state component

---
+++ src/app/modules/shared/components/empty-state/empty-state.ts
@@ -0,0 +1,44 @@
+import {Component, EventEmitter, Input, OnDestroy, OnInit, Output} from '@angular/core';
+import {Router} from '@angular/router';
+
+@Component({
+  selector: 'app-empty-state',
+  styleUrls: ['./empty-state.scss'],
+  templateUrl: './empty-state.html'
+})
+export class EmptyStateComponent implements OnInit, OnDestroy {
+
+  @Input()
+  public title: string;
+
+  @Input()
+  public icon: string;
+
+  @Input()
+  public ctaText: string;
+
+  @Input()
+  public ctaIcon: string;
+
+  @Input()
+  public ctaLink: string;
+
+  @Output()
+  public ctaClick: EventEmitter<any>;
+
+  constructor(private router: Router) {
+  }
+
+  public click() {
+    if (this.ctaLink) {
+      this.router.navigateByUrl(this.ctaLink);
+    }
+    this.ctaClick.emit();
+  }
+
+  ngOnInit(): void {
+  }
+
+  ngOnDestroy() {
+  }
+}
