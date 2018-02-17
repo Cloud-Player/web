0a09f1067fdb8d5e8e2ae9f5f634816b22971fd1
Zarlex <alex.zarges@online.de>
2018-02-17 20:12:38 +0100

    Add utils class with static methods

---
+++ src/app/modules/shared/src/utils.class.ts
@@ -0,0 +1,20 @@
+export class Utils {
+  static vendorPrefixes = ['', 'webkit', 'moz', 'ms'];
+  static capitalize(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  }
+
+  static toCamelCase(...args) {
+    let camelCaseStr = '';
+    let firstWord = true;
+    args.forEach((arg: string, index) => {
+      if (arg.length > 0 && firstWord) {
+        camelCaseStr += arg;
+        firstWord = false;
+      } else {
+        camelCaseStr += Utils.capitalize(arg);
+      }
+    });
+    return camelCaseStr;
+  }
+}
