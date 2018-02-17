9b7a1c7787447a0234cf810a90e320de9d83fe78
Zarlex <alex.zarges@online.de>
2018-02-17 20:24:52 +0100

    Clean up old code

--- src/app/modules/session/components/authenticated-user-view/authenticated-user-view.component.ts
+++ src/app/modules/session/components/authenticated-user-view/authenticated-user-view.component.ts
@@ -1,5 +1,4 @@
 import {Component} from '@angular/core';
-import {AuthService} from '../../../shared/services/auth.service';
 
 @Component({
   selector: 'app-authenticated-user-view',
@@ -7,11 +6,11 @@ import {AuthService} from '../../../shared/services/auth.service';
   templateUrl: './authenticated-user-view.template.html'
 })
 export class AuthenticatedUserViewComponent {
-  constructor(private authService: AuthService) {
+  constructor() {
 
   }
 
   disconnect(): void {
-    this.authService.disconnect();
+    // this.authService.disconnect();
   }
 }
