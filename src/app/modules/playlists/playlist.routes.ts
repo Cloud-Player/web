e5659e52023ae0e30f6baabed5cd20632e270b83
Zarlex <alex.zarges@online.de>
2018-02-17 20:25:18 +0100

    Update routes

--- src/app/modules/playlists/playlist.routes.ts
+++ src/app/modules/playlists/playlist.routes.ts
@@ -1,9 +1,19 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {PlayListViewComponent} from './components/playlist-view/playlist-view.component';
+import {AuthenticatedUserPlaylistGuard} from './guards/authenticated-user-playlist-guard';
 
 const routes: Routes = [
-  {path: 'playlists/:id', component: PlayListViewComponent}
+  {
+    path: 'playlists',
+    component: PlayListViewComponent,
+    canActivate: [AuthenticatedUserPlaylistGuard]
+  },
+  {
+    path: 'playlists/:provider/:id',
+    component: PlayListViewComponent,
+    canActivate: [AuthenticatedUserPlaylistGuard]
+  }
 ];
 
 @NgModule({
--- src/app/modules/session/session.routes.ts
+++ src/app/modules/session/session.routes.ts
@@ -1,17 +1,14 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
-import {SoundcloudCallbackComponent} from './components/soundcloud-callback/soundcloud_callback.component';
-import {LikedTracksViewComponent} from './components/liked-tracks-view/liked-tracks-view.component';
-import {UserPlayListViewComponent} from './components/user-playlist-view/user-playlist-view.component';
+import {FavouriteTracksViewComponent} from './components/favourite-tracks-view/favorite-tracks-view';
 import {AuthenticatedUserPlaylistsViewComponent} from './components/authenticated-user-playlists-view/authenticated-user-playlists-view.component';
 import {AuthenticatedUserViewComponent} from './components/authenticated-user-view/authenticated-user-view.component';
+import {AuthenticatedUserGuard} from '../shared/guards/authenticated-user-guard';
 
 const routes: Routes = [
-  {path: 'connect', component: SoundcloudCallbackComponent},
-  {path: 'me', component: AuthenticatedUserViewComponent},
-  {path: 'me/likes', component: LikedTracksViewComponent},
-  {path: 'me/playlists', component: AuthenticatedUserPlaylistsViewComponent},
-  {path: 'me/playlists/:id', component: UserPlayListViewComponent}
+  {path: 'me', component: AuthenticatedUserViewComponent, canActivate: [AuthenticatedUserGuard]},
+  {path: 'me/likes', component: FavouriteTracksViewComponent, canActivate: [AuthenticatedUserGuard]},
+  {path: 'me/playlists', component: AuthenticatedUserPlaylistsViewComponent, canActivate: [AuthenticatedUserGuard]}
 ];
 
 @NgModule({
