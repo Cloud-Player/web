966e30a74ec87377421eec35a20b811df08bc297
Zarlex <alex.zarges@online.de>
2018-02-26 22:26:02 +0100

    Remove old server part

--- server/connect/vars.php
+++
@@ -1,7 +0,0 @@
-<?php
-  $soundcloud = array(
-    'clientSecret' => YOUR SOUNDCLOUD CLIENT SECRET GOES HERE,
-    'clientId' => YOUR SOUNDCLOUD CLIENT ID GOES HERE,
-    'redirectUrl' => YOUR REDIRECT ULR GOES HERE
-);
-?>
