3b3da378c8be425bf367034595a9c2f4e986554f
Zarlex <alex.zarges@online.de>
2018-03-06 22:02:22 +0100

    Update toast animations and change style

--- src/app/modules/shared/components/toasts/toasts.ts
+++ src/app/modules/shared/components/toasts/toasts.ts
@@ -1,21 +1,43 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, ElementRef, OnInit} from '@angular/core';
 import {ToastService} from '../../services/toast';
 import {Toasts} from '../../collections/toasts';
 import {ToastModel} from '../../models/toast';
+import {LayoutChangeTypes, LayoutService, WindowElementTypes} from '../../services/layout';
+import {animate, style, transition, trigger} from '@angular/animations';
 
 @Component({
   selector: 'app-toasts',
   styleUrls: ['./toasts.scss'],
-  templateUrl: './toasts.html'
+  templateUrl: './toasts.html',
+  animations: [
+    trigger('toastAnimation', [
+      transition('void => *', [
+        style({transform: 'translateX(100%)'}),
+        animate('0.4s 0.1s ease-in-out', style({transform: 'translateX(0)'}))
+      ]),
+      transition('* => void', [
+        animate('0.3s 0.1s ease-out', style({transform: 'translateX(100%)'}))
+      ])
+    ])
+  ]
 })
 export class ToastsComponent implements OnInit {
   public toasts: Toasts<ToastModel>;
 
-  constructor(private toastService: ToastService) {
+  constructor(private el: ElementRef, private toastService: ToastService, private layoutService: LayoutService) {
 
   }
 
   ngOnInit(): void {
     this.toasts = this.toastService.getToasts();
+    this.layoutService.getObservable()
+      .filter(ev => ev.changeType === LayoutChangeTypes.windowElementChange)
+      .subscribe((ev) => {
+        if (this.layoutService.hasWindowElement(WindowElementTypes.MusicPlayer)) {
+          this.el.nativeElement.classList.add('music-player-active');
+        } else {
+          this.el.nativeElement.classList.remove('music-player-active');
+        }
+      });
   }
 }
