677a1ea439ecdfdb39f065491941aa34050a23f0
Zarlex <alex.zarges@online.de>
2018-03-08 13:12:09 +0100

    Set applicable accounts on init and track change

--- src/app/modules/authenticated-user/components/authenticated-user-playlist-selector-modal/authenticated-user-playlist-selector-modal.ts
+++ src/app/modules/authenticated-user/components/authenticated-user-playlist-selector-modal/authenticated-user-playlist-selector-modal.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {ChangeDetectorRef, Component, Input, NgZone, OnChanges, OnInit, SimpleChanges} from '@angular/core';
 import {AuthenticatedUserAccountsCollection} from '../../../api/authenticated-user/account/authenticated-user-accounts.collection';
 import {AuthenticatedUserModel} from '../../../api/authenticated-user/authenticated-user.model';
 import {IAuthenticatedUserAccount} from '../../../api/authenticated-user/account/authenticated-user-account.interface';
@@ -7,26 +7,31 @@ import {ProviderMap} from '../../../shared/src/provider-map.class';
 import {Subscription} from 'rxjs/Subscription';
 import {IModal, IModalComponent, IModalOptions} from '../../../shared/src/modal.interface';
 import {ITrack} from '../../../api/tracks/track.interface';
-import {IPlaylistItems} from '../../../api/playlists/playlist-item/playlist-items.interface';
 import {IPlaylist} from '../../../api/playlists/playlist.interface';
+import {ExternalUserAuthenticator} from '../../services/external-authenticator.class';
+import {Modal, ModalService} from '../../../shared/services/modal';
+import {AuthenticatedUserPlaylistFormViewComponent} from '../authenticated-user-playlist-form-view/authenticated-user-playlist-form-view';
+import {AuthenticatedUserPlaylistFormComponent} from '../authenticated-user-form-component/authenticated-user-playlist-form';
 
 @Component({
   selector: 'app-authenticated-user-playlist-selector-modal',
   styleUrls: ['./authenticated-user-playlist-selector-modal.scss'],
   templateUrl: './authenticated-user-playlist-selector-modal.html'
 })
-export class AuthenticatedUserPlaylistSelectorModalComponent implements OnInit, IModalComponent {
+export class AuthenticatedUserPlaylistSelectorModalComponent implements OnInit, OnChanges, IModalComponent {
   private _subscription: Subscription;
   private _modal: IModal;
+  private _createNewPlaylistModal: Modal<AuthenticatedUserPlaylistFormComponent>;
   public modalOptions: IModalOptions;
   public availableProviderMap = ProviderMap.map;
   public accounts: AuthenticatedUserAccountsCollection<IAuthenticatedUserAccount>;
   public selectedAccount: IAuthenticatedUserAccount;
+  public applicableAccounts: Array<IAuthenticatedUserAccount>;
 
   @Input()
   track: ITrack;
 
-  constructor() {
+  constructor(private externalUserAuthenticator: ExternalUserAuthenticator, private modalService: ModalService) {
     this.accounts = AuthenticatedUserModel.getInstance().accounts;
     this._subscription = new Subscription();
     this.modalOptions = {
@@ -52,23 +57,49 @@ export class AuthenticatedUserPlaylistSelectorModalComponent implements OnInit, 
     this._modal.hide();
   }
 
-  public getAccounts() {
-    const accounts = [];
+  public setAccounts() {
+    this.applicableAccounts = [];
     if (this.track) {
       this.accounts.each((account) => {
-        if (account.provider === this.track.provider || account.provider === 'cloudplayer') {
-          accounts.push(account);
+        if (
+          account.provider === this.track.provider || account.provider === 'cloudplayer'
+        ) {
+          this.applicableAccounts.push(account);
         }
       });
     }
-    return accounts;
+  }
+
+  public connect() {
+    this.externalUserAuthenticator.connect(this.selectedAccount)
+      .then(this.selectedAccount.playlists.fetch);
+  }
+
+  public createNewPlaylist() {
+    this._createNewPlaylistModal.getInstance().account = this.selectedAccount;
+    this._createNewPlaylistModal.getInstance().setPlaylistFromProvider(this.selectedAccount.provider);
+    this._createNewPlaylistModal.open();
+  }
+
+  ngOnInit(): void {
+    this.selectedAccount = this.accounts.getAccountForProvider('cloudplayer');
+  }
+
+  public ngOnChanges(changes: SimpleChanges): void {
+    if (changes.track) {
+      this.setAccounts();
+    }
   }
 
   setModal(modal: IModal) {
     this._modal = modal;
   }
 
-  ngOnInit(): void {
-    this.selectedAccount = this.accounts.getAccountForProvider('cloudplayer');
+  modalOnOpen(): void {
+    this._createNewPlaylistModal = this.modalService.createModal(AuthenticatedUserPlaylistFormComponent);
+  }
+
+  modalOnClose(): void {
+    this._createNewPlaylistModal.destroy();
   }
 }
