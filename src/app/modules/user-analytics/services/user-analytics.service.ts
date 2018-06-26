486cc2b59e87623eb65f41e77d92f49a96e5564b
Zarlex <alex.zarges@online.de>
2018-06-26 15:29:39 +0200

    Put events and pages in queue when service is disabled and flush it on activation

--- src/app/modules/user-analytics/services/user-analytics.service.ts
+++ src/app/modules/user-analytics/services/user-analytics.service.ts
@@ -5,7 +5,21 @@ import {TrackingProviders} from '../collections/tracking-providers.collection';
 @Injectable()
 export class UserAnalyticsService {
   private _trackingProviders: TrackingProviders<TrackingProviderModel> = new TrackingProviders();
-  private _isActive = true;
+  private _isActive = false;
+  private _trackPageQueue: Array<string> = [];
+  private _trackEventQueue: Array<{ eventName: string, eventAction: string, msg?: string }> = [];
+
+  private flushQueues() {
+    this._trackPageQueue.forEach((page) => {
+      this.trackPage(page);
+    });
+    this._trackPageQueue = [];
+
+    this._trackEventQueue.forEach((event) => {
+      this.trackEvent(event.eventName, event.eventAction, event.msg);
+    });
+    this._trackEventQueue = [];
+  }
 
   public addProvider(provider: TrackingProviderModel): void {
     this._trackingProviders.add(provider);
@@ -14,12 +28,20 @@ export class UserAnalyticsService {
   public trackEvent(eventName: string, eventAction: string, msg?: string) {
     if (this._isActive) {
       this._trackingProviders.trackEvent(eventName, eventAction, msg);
+    } else {
+      this._trackEventQueue.push({
+        eventName: eventName,
+        eventAction: eventAction,
+        msg: msg
+      });
     }
   }
 
   public trackPage(page: string) {
     if (this._isActive) {
       this._trackingProviders.trackPage(page);
+    } else {
+      this._trackPageQueue.push(page);
     }
   }
 
@@ -33,5 +55,8 @@ export class UserAnalyticsService {
 
   public setActive(isActive: boolean) {
     this._isActive = isActive;
+    if (isActive) {
+      this.flushQueues();
+    }
   }
 }
