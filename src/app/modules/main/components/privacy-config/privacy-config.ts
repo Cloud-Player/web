fe773824f2f5f75a96bfccf405e0a8fad1f5d691
Zarlex <alex.zarges@online.de>
2018-06-26 15:18:05 +0200

    Add privacy settings configurator

---
+++ src/app/modules/main/components/privacy-config/privacy-config.ts
@@ -0,0 +1,71 @@
+import {Component, Injectable, OnInit} from '@angular/core';
+import {ModalComponent} from '../../../shared/components/modal/modal/modal';
+import {Modal} from '../../../shared/src/modal-factory.class';
+import {IModal, IModalComponent, IModalOptions} from '../../../shared/src/modal.interface';
+import {PrivacyComponent} from '../privacy/privacy';
+import {ModalService} from '../../../shared/services/modal';
+import {IPrivacySettings} from '../main/main.component';
+import * as localforage from 'localforage';
+import {AuthenticatedUserModel} from '../../../api/authenticated-user/authenticated-user.model';
+import {PrivacyManager} from '../../services/privacy-manager';
+
+@Component({
+  selector: 'app-privacy-config',
+  templateUrl: './privacy-config.html',
+  styleUrls: ['./privacy-config.scss']
+})
+export class PrivacyConfigComponent implements IModalComponent, OnInit {
+  modalOptions: IModalOptions = {
+    title: 'Privacy Notice',
+    dismissible: false,
+    primaryAction: {
+      text: 'Save and continue',
+      action: () => {
+        this.privacyManger.save();
+      }
+    }
+    // secondaryAction: {
+    //   text: 'Delete my account'
+    // }
+  };
+  public idCookieIsRequired = true;
+  public privacySettings: IPrivacySettings;
+
+  constructor(private modalService: ModalService, private privacyManger: PrivacyManager) {
+  }
+
+  public openPrivacyStatement() {
+    this.modalService.createModalAsync(PrivacyComponent).then((modal) => {
+      modal.open();
+    });
+  }
+
+
+  modalOnOpen(): void {
+    this.setIdCookieIsRequired();
+  }
+
+  public ngOnInit(): void {
+    this.privacyManger.getPrivacySettings().then((settings) => {
+      this.privacySettings = settings;
+    });
+  }
+
+  public setIdCookieIsRequired(): void {
+    if (AuthenticatedUserModel.getInstance().accounts.getAccountForProvider('cloudplayer').isConnected()) {
+      this.idCookieIsRequired = true;
+    }
+  }
+}
+
+@Injectable()
+export class PrivacyConfigModalOpener {
+  constructor(private modalService: ModalService) {
+  }
+
+  open(): void {
+    this.modalService.createModalAsync(PrivacyConfigComponent).then((modal) => {
+      modal.open();
+    });
+  }
+}
