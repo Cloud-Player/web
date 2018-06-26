035e4faa0cae42b57e4dea2435dfe19cb1629c84
Zarlex <alex.zarges@online.de>
2018-06-26 15:25:42 +0200

    Remove localhost tracking deactivation

--- src/app/modules/user-analytics/user-analytics.module.ts
+++ src/app/modules/user-analytics/user-analytics.module.ts
@@ -24,11 +24,5 @@ export class UserAnalyticsModule {
         }
         userAnalyticsService.trackPage(currentRoute.snapshot.routeConfig.path);
       });
-
-    // Disable tracking for localhost
-    const currentOrigin = document.location.origin;
-    if (currentOrigin && currentOrigin.match(/localhost/)) {
-      userAnalyticsService.setActive(false);
-    }
   }
 }
