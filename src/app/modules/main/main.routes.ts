839df9fbd65c9c35da1c214e1718eff86a11a455
Zarlex <alex.zarges@online.de>
2018-06-26 15:37:31 +0200

    Add routes

--- src/app/modules/main/main.routes.ts
+++ src/app/modules/main/main.routes.ts
@@ -1,7 +1,11 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
+import {PrivacyComponent} from './components/privacy/privacy';
+import {ImprintComponent} from './components/imprint/imprint';
 
 const routes: Routes = [
+  {path: 'imprint', component: ImprintComponent},
+  {path: 'privacy', component: PrivacyComponent},
   {path: '', redirectTo: '/search', pathMatch: 'full'}
 ];
 
