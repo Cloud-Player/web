71fdfa04d6d62fe1c351d3bacc2a41720ee5c9ad
Zarlex <alex.zarges@online.de>
2018-06-26 15:17:47 +0200

    Add privacy and imprint page

---
+++ src/app/modules/main/components/privacy/privacy.ts
@@ -0,0 +1,50 @@
+import {Component} from '@angular/core';
+import {IModalComponent, IModalOptions} from '../../../shared/src/modal.interface';
+import * as localforage from 'localforage';
+import {IPrivacySettings} from '../main/main.component';
+import {UserAnalyticsService} from '../../../user-analytics/services/user-analytics.service';
+import {PrivacyManager} from '../../services/privacy-manager';
+
+@Component({
+  selector: 'app-privacy',
+  templateUrl: './privacy.html',
+  styleUrls: ['./privacy.scss']
+})
+export class PrivacyComponent implements IModalComponent {
+  modalOptions: IModalOptions = {
+    dismissible: true,
+    title: this.getTitle()
+  };
+
+  public isModal = false;
+
+  constructor(private privacyManager: PrivacyManager) {
+  }
+
+  public setModal() {
+    this.isModal = true;
+  }
+
+  public getTitle() {
+    if (this.isGerman()) {
+      return 'Datenschutzerklärung';
+    } else {
+      return 'Privacy Statement';
+    }
+  }
+
+  public isGerman() {
+    return navigator.language.match(/de/i);
+  }
+
+  public getMail(): string {
+    return atob('aGVsbG9AY2xvdWQtcGxheWVyLmlv');
+  }
+
+  public disableTracking(): void {
+    this.privacyManager.getPrivacySettings().then((settings) => {
+      settings.allowTracking = false;
+      this.privacyManager.save();
+    });
+  }
+}
