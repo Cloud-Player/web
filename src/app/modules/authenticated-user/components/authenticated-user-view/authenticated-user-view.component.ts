fe3f31e008ab320bf4978f3b19ab2b2bcb93062a
Zarlex <alex.zarges@online.de>
2018-06-26 15:24:52 +0200

    Add button to configure privacy settings and prepare delete account button

--- src/app/modules/authenticated-user/components/authenticated-user-view/authenticated-user-view.component.ts
+++ src/app/modules/authenticated-user/components/authenticated-user-view/authenticated-user-view.component.ts
@@ -1,22 +1,33 @@
-import {Component} from '@angular/core';
+import {Component, OnDestroy} from '@angular/core';
 import {IAuthenticatedUserAccount} from '../../../api/authenticated-user/account/authenticated-user-account.interface';
 import {ExternalUserAuthenticator} from '../../services/external-authenticator.class';
 import {AuthenticatedUserModel} from '../../../api/authenticated-user/authenticated-user.model';
 import {ProviderMap} from '../../../shared/src/provider-map.class';
+import {PrivacyConfigComponent, PrivacyConfigModalOpener} from '../../../main/components/privacy-config/privacy-config';
+import {filter} from 'rxjs/internal/operators';
+import {Modal, ModalStates} from '../../../shared/src/modal-factory.class';
+import {DeleteAccountComponent} from '../delete-account/delete-account';
+import {ModalComponent} from '../../../shared/components/modal/modal/modal';
+import {ModalService} from '../../../shared/services/modal';
+import {PrivacyManager} from '../../../main/services/privacy-manager';
 
 @Component({
   selector: 'app-authenticated-user-view',
   styleUrls: ['./authenticated-user-view.style.scss'],
   templateUrl: './authenticated-user-view.template.html'
 })
-export class AuthenticatedUserViewComponent {
+export class AuthenticatedUserViewComponent implements OnDestroy {
   public accounts: Array<IAuthenticatedUserAccount>;
   public authenticatedUser: AuthenticatedUserModel;
   public cloudPlayerAccount: IAuthenticatedUserAccount;
   public hasConnectedAccount: boolean;
   public providerMap = ProviderMap.map;
+  private _deleteAccountModal: Modal<DeleteAccountComponent>;
 
-  constructor(private externalUserAuthenticator: ExternalUserAuthenticator) {
+  constructor(private modalService: ModalService,
+              private privacyManager: PrivacyManager,
+              private externalUserAuthenticator: ExternalUserAuthenticator,
+              private privacyConfigModalOpener: PrivacyConfigModalOpener) {
     this.authenticatedUser = AuthenticatedUserModel.getInstance();
     this.cloudPlayerAccount = this.authenticatedUser.accounts.getAccountForProvider('cloudplayer');
     this.accounts = this.authenticatedUser.accounts.filter((account) => {
@@ -30,6 +41,7 @@ export class AuthenticatedUserViewComponent {
         return true;
       }
     });
+    this._deleteAccountModal = this.modalService.createModal(DeleteAccountComponent);
   }
 
   public connect(provider: string) {
@@ -37,5 +49,17 @@ export class AuthenticatedUserViewComponent {
     if (account) {
       this.externalUserAuthenticator.connect(account);
     }
+  }
+
+  public openPrivacySettings() {
+    this.privacyConfigModalOpener.open();
+  }
+
+  public deleteAccount() {
+    this._deleteAccountModal.open();
+  }
+
+  public ngOnDestroy(): void {
+    this._deleteAccountModal.destroy();
   }
 }
