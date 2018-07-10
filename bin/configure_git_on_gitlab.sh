ecb6fb81273d1cb25810894b9b248213d8a556d5
Zarlex <alex.zarges@online.de>
2018-07-10 17:40:11 +0200

    Change git conf

--- bin/configure_git_on_gitlab.sh
+++ bin/configure_git_on_gitlab.sh
@@ -1,15 +1,19 @@
 #!/usr/bin/env bash
 set -e
 
+REPO_REF="gitlab.com/auxapp/web"
 RELEASE_GIT_NAME="Bob Builder"
 RELEASE_GIT_MAIL="hello@aux.app"
 
-echo "$GIT_SSH_PRIV_KEY"
+echo "START"
+echo "https://$CI_DEPLOY_USER:$CI_DEPLOY_PASSWORD@$REPO_REF.git"
 
-which ssh-agent || ( apt-get update -y && apt-get install openssh-client -y )
-eval $(ssh-agent -s)
-ssh-add <(echo "$GIT_SSH_PRIV_KEY")
-git config --global user.email "${RELEASE_GIT_MAIL}"
-git config --global user.name "${RELEASE_GIT_NAME}"
-mkdir -p ~/.ssh
-cat gitlab-known-hosts >> ~/.ssh/known_hosts
+# Set or add the remote url for the github repo with the GH_TOKEN
+# The GH_TOKEN is a github personal access token https://github.com/settings/tokens
+# It is encrypted with travis `$ travis encrypt GH_TOKEN=<GH_PERSONAL_TOKEN>` and set as global env via the .travis.yml
+if git remote | grep origin > /dev/null
+then
+  git remote set-url origin https://$CI_DEPLOY_USER:$CI_DEPLOY_PASSWORD@$REPO_REF.git
+else
+  git remote add origin https://$GH_TOKEN@$GH_REF.git
+fi
