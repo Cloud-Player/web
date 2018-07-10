49462202b32f6c621979549d29cd3d4341e7ac6f
Zarlex <alex.zarges@online.de>
2018-07-10 19:15:18 +0200

    Change git conf

--- bin/configure_git_on_gitlab.sh
+++ bin/configure_git_on_gitlab.sh
@@ -9,8 +9,10 @@ which ssh-agent || ( apt-get update -y && apt-get install openssh-client -y )
 eval $(ssh-agent -s)
 
 # load the private key, which is accesible as a environment variable
-echo "$GIT_SSH_PRIV_KEY" | ssh-add -
 mkdir -p ~/.ssh
+echo "$GIT_SSH_PRIV_KEY" > ~/.ssh/id_rsa
+chmod 700 ~/.ssh/id_rsa
+ssh-add ~/.ssh/id_rsa
 
 # ensure that ssh will trust a new host, instead of asking
 echo -e "Host *\n\tStrictHostKeyChecking no\n\n" > ~/.ssh/config
