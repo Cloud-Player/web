548a2afb3748546df90e47077c71b5a2972494ae
Zarlex <alex.zarges@online.de>
2018-07-10 19:41:47 +0200

    Clean up file

--- bin/release_on_gitlab.sh
+++ bin/release_on_gitlab.sh
@@ -1,46 +1,13 @@
 #!/usr/bin/env bash
 set -e
 
-echo "0"
-
-RELEASE_GIT_NAME="Bob Builder"
-RELEASE_GIT_MAIL="hello@aux.app"
-
-echo "01"
-
-CURRENT_GIT_USER=`git config user.name`
-CURRENT_GIT_USERMAIL=`git config user.email`
-CURRENT_BRANCH=`git rev-parse --abbrev-ref HEAD`
-
-echo "02"
-
-# Sets everything back to the beginning, before the release process has been started
-reset () {
-    git reset --hard origin/$CURRENT_BRANCH
-    git config user.name "$CURRENT_GIT_USER"
-    git config user.email "$CURRENT_GIT_USERMAIL"
-}
-
-echo "03"
-
 # Shows error message and exits with statuscode 1
 exit_with_error () {
     echo $1
     exit 1
 }
 
-echo "1"
-
 git fetch origin
-
-# This replaces the current commiter for the release
-git config user.name "$RELEASE_GIT_NAME"
-git config user.email "$RELEASE_GIT_MAIL"
-
-# Calls function when script exits (error and success)
-trap reset EXIT
-
-echo "2"
 
 # Get version number from package json version
 VERSION_NUMBER=$(cat package.json \
@@ -50,18 +17,12 @@ VERSION_NUMBER=$(cat package.json \
   | sed 's/[",]//g' \
   | xargs)
 
-echo "3"
-
 # Get changelog from current version. Version has to be mentioned in Changelog: `# vX.X.X`
 CHANGELOG=$(sed -n -e "/# v$VERSION_NUMBER/,/# v/ p" CHANGELOG.md \
   | sed -e '1d;$d')
 
-echo "4"
-
 # Ignore changes of package-lock.json
 git checkout -- package-lock.json
-
-echo "5"
 
 # Check if a tag with the same version already exists
 if [ "$(git ls-remote origin refs/tags/v${VERSION_NUMBER})" ]; then
@@ -69,22 +30,16 @@ if [ "$(git ls-remote origin refs/tags/v${VERSION_NUMBER})" ]; then
  exit 0;
 fi
 
-echo "6"
-
 # Check if there are any local changes that are not committed yet
 if [ -n "$(git status --porcelain)" ]; then
   git status --porcelain
   exit_with_error "There are changes that are not committed yet. Make sure you have checked in all changes before you run this script!";
 fi
 
-echo "7"
-
 # Check if all changes have been pushed to remote
 if [ "$(git log origin/$CURRENT_BRANCH..HEAD)" ]; then
   exit_with_error "Not all changes are pushed! Please push all changes before you run this script"
 fi
-
-echo "8"
 
 # Check if version is mentioned in CHANGELOG.md
 if [ "$CHANGELOG" == "" ] ; then
