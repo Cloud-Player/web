508be1e3e249a2130cb25cfb75e734c97aae0038
Zarlex <alex.zarges@online.de>
2018-07-10 16:36:26 +0200

    add scripts for gitlab pipeline

---
+++ bin/zip_version_gitlab.sh
@@ -0,0 +1,32 @@
+#/bin/sh
+set -e
+
+# Get version number from package json version
+VERSION_NUMBER=$(cat package.json \
+  | grep version \
+  | head -1 \
+  | awk -F: '{ print $2 }' \
+  | sed 's/[",]//g' \
+  | xargs)
+
+echo "##########################################"
+echo "#                                        #"
+echo "# Zip version ${VERSION_NUMBER}...       #"
+echo "#                                        #"
+echo "##########################################"
+
+# Create a zip file that is attached to the gitlab release
+mkdir -p zip
+cp -r dist zip/${VERSION_NUMBER}
+cd zip
+zip -r app.zip ${VERSION_NUMBER}
+mkdir -p public
+mv app.zip public/aux-app-v${VERSION_NUMBER}.zip
+rm -rf ${VERSION_NUMBER}
+cd ..
+
+echo "##########################################"
+echo "#                                        #"
+echo "# Version is zipped!                     #"
+echo "#                                        #"
+echo "##########################################"
