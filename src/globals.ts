3cea5b6f474c5ae042ad6ccbb3d888ff7f961cc4
Zarlex <alex.zarges@online.de>
2018-10-09 12:33:53 +0200

    Provide host as global

--- src/globals.ts
+++ src/globals.ts
@@ -1,8 +1,5 @@
-const soundCloudredirectDomain = 'https://sc.menu-flow.com';
-
 export const Globals = {
   soundcloudClientId: 'mqCVRxAeYDUjlPhqj27Hb1H9kydm9fMm',
-  soundcloudRedirectDomain: soundCloudredirectDomain,
-  soundcloudRedirectUrl: soundCloudredirectDomain + '/connect',
+  apiHost: 'https://api.aux.app',
   youtubeClientId: 'AIzaSyChl_ZzHjy7qX1A48q-P63SjffSHVvb9aE'
 };
